6ea22cc84c44ec605f843a26a99a8851b2353438
Romke van Dijk <github.com@ocbios.nl>
2023-07-20 17:15:49 +0200

    Implementing dynamic backend url Fixes #928

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+# Check if environment variable HASHTOPOLIS_BACKEND_URL is set
+# and use it with envsubst to set the backend url in the config.json
+if [ -n "$HASHTOPOLIS_BACKEND_URL" ]; then
+  echo "Using HASHTOPOLIS_BACKEND_URL: $HASHTOPOLIS_BACKEND_URL"
+  envsubst < /usr/share/nginx/html/assets/config.json.example > /usr/share/nginx/html/assets/config.json
+fi
+
+echo "Done setting up Hashtopolis frontend. Starting nginx..."
 nginx -g 'daemon off;'
