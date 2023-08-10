f36d01821ee975d6677f2db33d15c323b87742ce
Benito Díaz-Medina <benitodm90@gmail.com>
2023-08-10 07:52:40 +0100

    Change Button Action for 3 dots and Table style

--- src/app/shared/table/button-actions.component.ts
+++ src/app/shared/table/button-actions.component.ts
@@ -1,19 +1,22 @@
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-button-actions',
   template: `
-  <td class="overflow-hidden align-center">
-    <div ngbDropdown container="body">
-      <button type="button" class="btn btn-sm btn-gray-800 btn-sm btn-actions" ngbDropdownToggle>Actions</button>
-      <div ngbDropdownMenu>
-        <div #content><ng-content></ng-content></div>
-      </div>
+  <div ngbDropdown container="body">
+    <button type="button" class="navbar-toggler btn-actions"  data-mdb-toggle="collapse" data-mdb-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+      aria-expanded="false" aria-label="Toggle navigation" ngbDropdownToggle>
+      <fa-icon [icon]="faEllipsisH" aria-hidden="true"></fa-icon>
+    </button>
+    <div ngbDropdownMenu>
+      <div #content><ng-content></ng-content></div>
     </div>
-  </td>
+  </div>
   `
 })
 export class ButtonActionsComponent  {
+  faEllipsisH=faEllipsisH;
 
   constructor() { }
 
