3a6677c24b5e9ca4a03db48aa15733fa6931b7bc
BenM <benitodm90@gmail.com>
2023-09-01 14:22:20 +0100

    Speed calculation for Tasks and AgentId (#11)
    * Speed calculation for Tasks and AgentId
    
    Changing MaxResults from 60000 to config value that is a default 3000. I'm not sure what is the sweet spot value for this.
    
    * Update agents-speed.pipe.ts
    
    ---------
    
    Co-authored-by: Romke van Dijk <github.com@ocbios.nl>

--- src/app/core/_pipes/agents-speed.pipe.ts
+++ src/app/core/_pipes/agents-speed.pipe.ts
@@ -35,8 +35,7 @@ export class AgentsSpeedPipe implements PipeTransform {
 
   transform(id: number, type?:boolean){
 
-      // const maxResults = environment.config.prodApiMaxResults;
-      const maxResults = 60000;
+      const maxResults = environment.config.prodApiMaxResults;
       const chunktime = this.uiService.getUIsettings('chunktime').value;
       const cspeed = [];
       let params: any;
