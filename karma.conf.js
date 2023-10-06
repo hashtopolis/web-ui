83328103d1c63a801fb867f705dea959628f2213
itbf9 <itbf@proton.me>
2023-10-06 14:14:45 +0200

    Add puppeteer to handle chrome browser

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 
 module.exports = function (config) {
   config.set({
@@ -41,11 +43,5 @@ module.exports = function (config) {
     browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true,
-    customLaunchers: {
-      ChromeHeadlessCustom: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu']
-      }
-    },
   });
 };
