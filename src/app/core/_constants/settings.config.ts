9bc99335e2cde0eb60d1f7e5cf63a37bf43125da
itbf9 <itbf@proton.me>
2023-10-10 20:21:59 +0200

    format code

--- src/app/core/_constants/settings.config.ts
+++ src/app/core/_constants/settings.config.ts
@@ -3,15 +3,15 @@
 **/
 
 export const dateFormat = [
-  {format:'d/M/yy', description:'d/M/yy (ie. 6/7/23 )'},
-  {format:'dd/MM/yyyy h:mm:ss', description:'dd/MM/yyyy h:mm:ss (ie. 06/07/2023, 9:03 AM)'},
-  {format:'d MMM, y h:mm:ss a', description:'dd/MM/yyyy h:mm:ss (ie. 06 Jul, 2023 9:03:01 AM)'},
-  {format:'M/d/yy', description:'M/d/yy (ie. 7/6/23)'},
-  {format:'M/d/yy, h:mm a', description:'M/d/yy, h:mm a (ie. 7/6/23, 9:03 AM)'},
-  {format:'MMM d, y, h:mm:ss a', description:'MMM d, y, h:mm:ss a (ie. Jul 06, 2023, 9:03:01 AM)'},
-  {format:'yy/M/d', description:'yy/M/d (ie. 23/7/6 )'},
-  {format:'yyyy/M/d, h:mm:ss a', description:'yyyy/M/d (ie. 2023/7/6, 9:03:01 AM )'},
-  {format:'yyyy/MM/dd h:mm:ss', description:'yyyy/MM/dd h:mm:ss (ie. 2023/07/06, 9:03 AM)'},
+  { format: 'd/M/yy', description: 'd/M/yy (ie. 6/7/23 )' },
+  { format: 'dd/MM/yyyy h:mm:ss', description: 'dd/MM/yyyy h:mm:ss (ie. 06/07/2023, 9:03 AM)' },
+  { format: 'd MMM, y h:mm:ss a', description: 'dd/MM/yyyy h:mm:ss (ie. 06 Jul, 2023 9:03:01 AM)' },
+  { format: 'M/d/yy', description: 'M/d/yy (ie. 7/6/23)' },
+  { format: 'M/d/yy, h:mm a', description: 'M/d/yy, h:mm a (ie. 7/6/23, 9:03 AM)' },
+  { format: 'MMM d, y, h:mm:ss a', description: 'MMM d, y, h:mm:ss a (ie. Jul 06, 2023, 9:03:01 AM)' },
+  { format: 'yy/M/d', description: 'yy/M/d (ie. 23/7/6 )' },
+  { format: 'yyyy/M/d, h:mm:ss a', description: 'yyyy/M/d (ie. 2023/7/6, 9:03:01 AM )' },
+  { format: 'yyyy/MM/dd h:mm:ss', description: 'yyyy/MM/dd h:mm:ss (ie. 2023/07/06 9:03 AM)' },
 ];
 
 
@@ -20,12 +20,12 @@ export const dateFormat = [
 **/
 
 export const serverlog = [
-  {id:0, value: 'TRACE'},
-  {id:10, value: 'DEBUG'},
-  {id:20, value: 'INFO'},
-  {id:30, value: 'WARNING'},
-  {id:40, value: 'ERROR'},
-  {id:50, value: 'FATAL'}
+  { id: 0, value: 'TRACE' },
+  { id: 10, value: 'DEBUG' },
+  { id: 20, value: 'INFO' },
+  { id: 30, value: 'WARNING' },
+  { id: 40, value: 'ERROR' },
+  { id: 50, value: 'FATAL' }
 ];
 
 /**
@@ -33,10 +33,10 @@ export const serverlog = [
 **/
 
 export const proxytype = [
-  {value:'HTTP'},
-  {value:'HTTPS'},
-  {value:'SOCKS4'},
-  {value:'SOCKS5'}
+  { value: 'HTTP' },
+  { value: 'HTTPS' },
+  { value: 'SOCKS4' },
+  { value: 'SOCKS5' }
 ];
 
 /**
