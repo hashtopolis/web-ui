9543c3ebda27a2e50c5da513dcf4cac5383cfed1
itbf9 <itbf@proton.me>
2023-10-10 21:16:41 +0200

    Update datepipe to use the new cookieservice

--- src/app/core/_pipes/date.pipe.ts
+++ src/app/core/_pipes/date.pipe.ts
@@ -1,60 +1,54 @@
-import {
-  Inject,
-  LOCALE_ID,
-  PipeTransform,
-  Pipe
-} from '@angular/core';
-import { CookieService } from '../_services/shared/cookies.service';
-import { dateFormat } from '../../core/_constants/settings.config';
+import { Inject, LOCALE_ID, PipeTransform, Pipe } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { CookieStorageService } from '../_services/storage/cookie-storage.service';
+import { UISettings, UiSettingsComponent } from 'src/app/account/settings/ui-settings/ui-settings.component';
 
 /**
- * Pipe to format date
- * @param epoch - Epoch date number
+ * Custom Angular pipe for formatting dates based on user settings.
+ *
  * Usage:
  *   value | uiDate
+ *
  * Example:
  *   {{ 1694866300 | uiDate }}
- * @returns 16/09/2023
-**/
-
+ *
+ * @returns Formatted date string, e.g., '16/09/2023'
+ */
 @Pipe({
   name: 'uiDate'
 })
 export class uiDatePipe extends DatePipe implements PipeTransform {
 
-  constructor(
-    private cookieService: CookieService,
-    @Inject(LOCALE_ID) locale: string
-  ) { super(locale); }
+  settings: UISettings
 
-  override transform(epoch: number):any {
-
-    if(epoch === undefined  || epoch === null) return epoch;
-
-    if(!this.cookieService.getCookie('localtimefmt')){
-      this.cookieService.setCookie('localtimefmt', 'dd/MM/yyyy h:mm:ss', 365);
-    }
-
-    const format = this.checkFormat(this.cookieService.getCookie('localtimefmt'));
-
-    return super.transform(epoch*1000, format);
+  /**
+   * Creates an instance of UiDatePipe.
+   *
+   * @param cookieService - The CookieStorageService for retrieving user interface settings.
+   * @param locale - The locale ID used for date formatting.
+   */
+  constructor(cookieService: CookieStorageService<UISettings>, @Inject(LOCALE_ID) locale: string) {
+    super(locale);
+    this.settings = cookieService.getItem(UiSettingsComponent.COOKIE_NAME)
   }
 
-  //Check that format is correct
-  checkFormat(format: any){
-    let res; //Default date format
-    for(let i=0; i < dateFormat.length; i++){
-      if(dateFormat[i]['format']== format){
-        res = format;
-      }
+  /**
+   * Transforms an epoch timestamp into a formatted date string based on user settings.
+   *
+   * @param epoch - The epoch timestamp to be formatted.
+   * @returns The formatted date string.
+   */
+  override transform(epoch: number): any {
+    if (epoch === undefined || epoch === null) {
+      return epoch;
     }
-    if(!res){
-      res = 'dd/MM/yyyy h:mm:ss';
-      this.cookieService.setCookie('localtimefmt', res, 365);
-    }
-    return res;
-  }
 
+    let format: string = UiSettingsComponent.DEFAULT_LOCALTIMEFMT;
+    if (this.settings) {
+      format = this.settings.localtimefmt
+    }
+
+    return super.transform(epoch * 1000, format);
+  }
 }
 
