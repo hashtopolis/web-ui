dba4f8616754d8d4283087a9d35114c4946e577e
Benito Díaz-Medina <benitodm90@gmail.com>
2023-10-23 14:47:09 +0100

    All users, add documentation

--- src/app/users/all-users/all-users.component.ts
+++ src/app/users/all-users/all-users.component.ts
@@ -1,15 +1,13 @@
 import { faEdit, faHomeAlt, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
-import Swal from 'sweetalert2/dist/sweetalert2.js';
 import { ActivatedRoute } from '@angular/router';
+import { Subject, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
-import { Subject } from 'rxjs';
 
-import { UIConfigService } from '../../core/_services/shared/storage.service';
+import { AutoTitleService } from 'src/app/core/_services/shared/autotitle.service';
 import { AlertService } from 'src/app/core/_services/shared/alert.service';
 import { GlobalService } from 'src/app/core/_services/main.service';
 import { environment } from './../../../environments/environment';
-import { PageTitle } from 'src/app/core/_decorators/autotitle';
 import { SERV } from '../../core/_services/main.config';
 import { DataTableDirective } from 'angular-datatables';
 
@@ -19,24 +17,28 @@ declare let $:any;
   selector: 'app-all-users',
   templateUrl: './all-users.component.html'
 })
-@PageTitle(['Show Users'])
-export class AllUsersComponent  implements OnInit, OnDestroy {
+/**
+ * AllUsersComponent is a component that manages and displays all users data.
+ *
+ * It uses DataTables to display and interact with the users tasks data, including exporting, deleting, bulk actions
+ * and refreshing the table.
+ */
+export class AllUsersComponent implements OnInit, OnDestroy {
 
-  faHome=faHomeAlt;
-  faTrash=faTrash;
-  faEdit=faEdit;
-  faPlus=faPlus;
+  // Font Awesome icons
+  faHome = faHomeAlt;
+  faTrash = faTrash;
+  faEdit = faEdit;
+  faPlus = faPlus;
 
+  // ViewChild reference to the DataTableDirective
   @ViewChild(DataTableDirective)
   dtElement: DataTableDirective;
 
   dtTrigger: Subject<any> = new Subject<any>();
   dtOptions: any = {};
 
-  ngOnDestroy(): void {
-    this.dtTrigger.unsubscribe();
-  }
-
+  // List of users ToDo. Change to interface
   public allusers: {
     id: number,
     name: string,
@@ -44,7 +46,7 @@ export class AllUsersComponent  implements OnInit, OnDestroy {
     lastLoginDate: number,
     email: string,
     isValid: boolean,
-    sessionLifetime:number,
+    sessionLifetime: number,
     rightGroupId: string,
     globalPermissionGroup: {
       name: string,
@@ -52,159 +54,49 @@ export class AllUsersComponent  implements OnInit, OnDestroy {
     }
   }[] = [];
 
+  // Subscriptions to unsubscribe on component destruction
+  subscriptions: Subscription[] = []
+
+  private maxResults = environment.config.prodApiMaxResults;
+
   constructor(
-    private uiService: UIConfigService,
-    private route:ActivatedRoute,
+    private titleService: AutoTitleService,
+    private route: ActivatedRoute,
     private alert: AlertService,
     private gs: GlobalService,
-    private router:Router
-  ) { }
-
-  private maxResults = environment.config.prodApiMaxResults
-
-  ngOnInit(): void {
-
-    const params = {'maxResults': this.maxResults, 'expand': 'globalPermissionGroup' }
-    this.gs.getAll(SERV.USERS,params).subscribe((users: any) => {
-      this.allusers = users.values;
-      this.dtTrigger.next(void 0);
-    });
-
-    const self = this;
-    this.dtOptions = {
-      dom: 'Bfrtip',
-      scrollX: true,
-      pageLength: 25,
-      lengthMenu: [
-          [10, 25, 50, 100, 250, -1],
-          [10, 25, 50, 100, 250, 'All']
-      ],
-      stateSave: true,
-      // "stateLoadParams": function (settings, data) {
-      //   return false;
-      // },
-      select: true,
-      buttons: {
-        dom: {
-          button: {
-            className: 'dt-button buttons-collection btn btn-sm-dt btn-outline-gray-600-dt',
-          }
-        },
-      buttons: [
-        {
-          text: '↻',
-          autoClose: true,
-          action: function (e, dt, node, config) {
-            self.onRefresh();
-          }
-        },
-        {
-          extend: 'collection',
-          text: 'Export',
-          buttons: [
-            {
-              extend: 'excelHtml5',
-              exportOptions: {
-                columns: [0, 1]
-              },
-            },
-            {
-              extend: 'print',
-              exportOptions: {
-                columns: [0,1]
-              },
-              customize: function ( win ) {
-                $(win.document.body)
-                    .css( 'font-size', '10pt' )
-                $(win.document.body).find( 'table' )
-                    .addClass( 'compact' )
-                    .css( 'font-size', 'inherit' );
-             }
-            },
-            {
-              extend: 'csvHtml5',
-              exportOptions: {modifier: {selected: true}},
-              select: true,
-              customize: function (dt, csv) {
-                let data = "";
-                for (let i = 0; i < dt.length; i++) {
-                  data = "Agents\n\n"+  dt;
-                }
-                return data;
-             }
-            },
-              'copy'
-            ]
-          },
-          {
-            extend: 'collection',
-            text: 'Bulk Actions',
-            className: 'dt-button buttons-collection btn btn-sm-dt btn-outline-gray-600-dt',
-            buttons: [
-                  {
-                    text: 'Delete Users',
-                    autoClose: true,
-                    action: function (e, dt, node, config) {
-                      self.onDeleteBulk();
-                    }
-                  }
-               ]
-            },
-        ],
-      }
-    };
+    private router: Router
+  ) {
+    titleService.set(['Show Users'])
   }
 
-  onRefresh(){
+  /**
+   * Initializes DataTable and retrieves pretasks.
+   */
+
+  ngOnInit(): void {
+    this.getUsers();
+    this.setupTable();
+  }
+
+  /**
+   * Unsubscribes from active subscriptions.
+   */
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
+    for (const sub of this.subscriptions) {
+      sub.unsubscribe();
+    }
+  }
+
+  // Refresh the data and the DataTable
+  onRefresh() {
     this.rerender();
     this.ngOnInit();
   }
 
-  editButtonClick(){
-    this.router.navigate(['edit'], {relativeTo: this.route});
-  }
-
-  onDelete(id: number, name: string){
-    this.alert.deleteConfirmation(name,'Users').then((confirmed) => {
-      if (confirmed) {
-        // Deletion
-        this.gs.delete(SERV.USERS, id).subscribe(() => {
-          // Successful deletion
-          this.alert.okAlert(`Deleted User ${name}`, '');
-          this.onRefreshTable(); // Refresh the table
-        });
-      } else {
-        // Handle cancellation
-        this.alert.okAlert(`User ${name} is safe!`,'');
-      }
-    });
-  }
-
-  onRefreshTable(){
-    setTimeout(() => {
-      this.ngOnInit();
-      this.rerender();  // rerender datatables
-    },2000);
-  }
-
-  onSelectedUsers(){
-    $(".dt-button-background").trigger("click");
-    const selection = $($(this.dtElement).DataTable.tables()).DataTable().rows({ selected: true } ).data().pluck(0).toArray();
-    if(selection.length == 0) {
-      this.alert.okAlert('You haven not selected any Group','');
-      return;
-    }
-    const selectionnum = selection.map(i=>Number(i));
-
-    return selectionnum;
-  }
-
-  async onDeleteBulk() {
-    const UserIds = this.onSelectedUsers();
-    this.alert.bulkDeleteAlert(UserIds,'Users',SERV.USERS);
-    this.onRefreshTable();
-  }
-
+  /**
+   * Rerender the DataTable.
+  */
   rerender(): void {
     this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
       dtInstance.destroy();
@@ -214,6 +106,178 @@ export class AllUsersComponent  implements OnInit, OnDestroy {
     });
   }
 
+  /**
+   * Fetches Users from the server.
+   * Subscribes to the API response and updates the Users list.
+  */
+  getUsers(): void {
+    // Set parameters for the API request
+    const params = { 'maxResults': this.maxResults, 'expand': 'globalPermissionGroup' };
+
+    // Make an API call to get users data
+    this.subscriptions.push(this.gs.getAll(SERV.USERS, params).subscribe((response: any) => {
+      this.allusers = response.values;
+      this.dtTrigger.next(void 0);
+    }));
+  }
+
+  /**
+   * Sets up the DataTable options and buttons.
+   * Customizes DataTable appearance and behavior.
+   */
+  setupTable(): void {
+    // DataTables options
+    const self = this;
+    this.dtOptions = {
+      dom: 'Bfrtip',
+      scrollX: true,
+      pageLength: 25,
+      lengthMenu: [
+        [10, 25, 50, 100, 250, -1],
+        [10, 25, 50, 100, 250, 'All']
+      ],
+      stateSave: true,
+      select: true,
+      buttons: {
+        dom: {
+          button: {
+            className: 'dt-button buttons-collection btn btn-sm-dt btn-outline-gray-600-dt',
+          }
+        },
+        buttons: [
+          {
+            text: '↻',
+            autoClose: true,
+            action: function (e, dt, node, config) {
+              self.onRefresh();
+            }
+          },
+          {
+            extend: 'collection',
+            text: 'Export',
+            buttons: [
+              {
+                extend: 'excelHtml5',
+                exportOptions: {
+                  columns: [0, 1]
+                },
+              },
+              {
+                extend: 'print',
+                exportOptions: {
+                  columns: [0, 1]
+                },
+                customize: function (win) {
+                  $(win.document.body)
+                    .css('font-size', '10pt');
+                  $(win.document.body).find('table')
+                    .addClass('compact')
+                    .css('font-size', 'inherit');
+                }
+              },
+              {
+                extend: 'csvHtml5',
+                exportOptions: { modifier: { selected: true } },
+                select: true,
+                customize: function (dt, csv) {
+                  let data = '';
+                  for (let i = 0; i < dt.length; i++) {
+                    data = 'Agents\n\n' + dt;
+                  }
+                  return data;
+                }
+              },
+              'copy'
+            ]
+          },
+          {
+            extend: 'collection',
+            text: 'Bulk Actions',
+            className: 'dt-button buttons-collection btn btn-sm-dt btn-outline-gray-600-dt',
+            buttons: [
+              {
+                text: 'Delete Users',
+                autoClose: true,
+                action: function (e, dt, node, config) {
+                  self.onDeleteBulk();
+                }
+              }
+            ]
+          },
+        ],
+      }
+    };
+  }
+
+  // Refresh the table after a delete operation
+  onRefreshTable() {
+    setTimeout(() => {
+      this.ngOnInit();
+      this.rerender();  // Rerender the DataTable
+    }, 2000);
+  }
+
+  /**
+   * Navigate to the edit page.
+   */
+  editButtonClick() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
+  }
+
+  /**
+   * Handles user deletion.
+   * Displays a confirmation dialog and deletes the user if confirmed.
+   *
+   * @param {number} id - The ID of the user to delete.
+   * @param {string} name - The name of the user.
+   */
+  onDelete(id: number, name: string) {
+    this.alert.deleteConfirmation(name, 'Users').then((confirmed) => {
+      if (confirmed) {
+        // Deletion
+        this.subscriptions.push(this.gs.delete(SERV.USERS, id).subscribe(() => {
+          // Successful deletion
+          this.alert.okAlert(`Deleted User ${name}`, '');
+          this.onRefreshTable(); // Refresh the table
+        }));
+      } else {
+        // Handle cancellation
+        this.alert.okAlert(`User ${name} is safe!`, '');
+      }
+    });
+  }
+
+  /**
+   * BULK ACTIONS
+   *
+  */
+
+  /**
+   * Handles pretask selection.
+   * On multi select grabs the ids to be used for bulk action
+   *
+  */
+  onSelectedUsers() {
+    $(".dt-button-background").trigger("click");
+    const selection = $($(this.dtElement).DataTable.tables()).DataTable().rows({ selected: true }).data().pluck(0).toArray();
+    if (selection.length == 0) {
+      this.alert.okAlert('You have not selected any Group', '');
+      return;
+    }
+    const selectionnum = selection.map(i => Number(i));
+
+    return selectionnum;
+  }
+
+  /**
+   * Handles bulk deletion
+   * Delete the pretasks showing a progress bar
+   *
+  */
+  async onDeleteBulk() {
+    const UserIds = this.onSelectedUsers();
+    this.alert.bulkDeleteAlert(UserIds, 'Users', SERV.USERS);
+    this.onRefreshTable();
+  }
 
 }
-
