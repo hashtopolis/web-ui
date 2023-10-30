47c5133c90c5d168dcfa3e4a6db216c4dd601152
itbf9 <itbf@proton.me>
2023-10-30 22:03:39 +0100

    New bulk action menu

---
+++ src/app/core/_components/menus/bulk-action-menu/bulk-action-menu.constants.ts
@@ -0,0 +1,14 @@
+export class BulkActionMenuLabel {
+  static readonly DELETE_AGENTS = 'Delete Agents';
+  static readonly ACTIVATE_AGENTS = 'Activate Agents';
+  static readonly DEACTIVATE_AGENTS = 'Deactivate Agents';
+  static readonly ARCHIVE_TASKS = 'Archive Tasks';
+  static readonly DELETE_TASKS = 'Delete Tasks';
+}
+
+export class BulkActionMenuAction {
+  static readonly DELETE = 'bulk-delete';
+  static readonly ACTIVATE = 'bulk-activate';
+  static readonly DEACTIVATE = 'bulk-deactivate';
+  static readonly ARCHIVE = 'bulk-archive';
+}
