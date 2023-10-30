d86f34f6a319a8ed1a60a4304404fafe56ce990b
itbf9 <itbf@proton.me>
2023-10-30 22:03:23 +0100

    New row action menu

---
+++ src/app/core/_components/menus/row-action-menu/row-action-menu.constants.ts
@@ -0,0 +1,19 @@
+export class RowActionMenuLabel {
+  static readonly EDIT_AGENT = 'Edit';
+  static readonly DELETE_AGENT = 'Delete';
+  static readonly EDIT_TASK = 'Edit';
+  static readonly COPY_TO_TASK = 'Copy to Task';
+  static readonly COPY_TO_PRETASK = 'Copy to Pretask';
+  static readonly EDIT_SUBTASKS = 'Edit Subtasks';
+  static readonly ARCHIVE_TASK = 'Archive';
+  static readonly DELETE_TASK = 'Delete';
+}
+
+export class RowActionMenuAction {
+  static readonly EDIT = 'edit';
+  static readonly DELETE = 'delete';
+  static readonly ARCHIVE = 'archive';
+  static readonly COPY_TO_TASK = 'copy-to-task';
+  static readonly COPY_TO_PRETASK = 'copy-to-pretask';
+  static readonly EDIT_SUBTASKS = 'edit-subtasks';
+}
