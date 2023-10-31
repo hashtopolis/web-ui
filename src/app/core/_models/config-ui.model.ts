8246b455da773bd961b078e7ee5832ccf4a99061
itbf9 <itbf@proton.me>
2023-10-31 10:58:26 +0100

    Remove unused interface

--- src/app/core/_models/config-ui.model.ts
+++ src/app/core/_models/config-ui.model.ts
@@ -1,21 +1,5 @@
-export class IStorage {
-  constructor(
-    private _timefmt: string,
-    private _enablebrain: number,
-    private _halias: string,
-    private _bchars: string,
-    private _chunkt: string,
-    private _statimer: string,
-    private _timestamp: number,
-    private _expiresin: number,
-  ) {}
-
-}
-
+/** Not used, remove when merged with https://github.com/hashtopolis/web-ui/pull/22 */
 export class UIGConfig {
-  constructor(
-
-  ) {}
 
 }
 
