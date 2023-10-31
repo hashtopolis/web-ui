9a90508f904aaabf9e3023cba91de82ebcfc23f7
itbf9 <itbf@proton.me>
2023-10-31 11:09:00 +0100

    New user interface

--- src/app/core/_models/user.model.ts
+++ src/app/core/_models/user.model.ts
@@ -1,17 +1,36 @@
 export interface BaseUser {
-  userId: number;
-  username: string;
-  email: string;
-  rightGroupId: number;
-  registered: number;
-  lastLogin: number;
-  isValid: number;
-  sessionLifetime: number;
+  userId: number
+  username: string
+  email: string
+  rightGroupId: number
+  registered: number
+  lastLogin: number
+  isValid: number
+  sessionLifetime: number
 }
 
 export interface CreateUser extends BaseUser {
-  username: string;
-  email: string;
-  rightGroupId: number;
-  isAdmin: number;
+  username: string
+  email: string
+  rightGroupId: number
+  isAdmin: number
+}
+
+export interface User {
+  _id: number
+  _self: string
+  email: string
+  globalPermissionGroupId: number
+  id?: number
+  isComputedPassword: boolean
+  isValid: boolean
+  lastLoginDate: number
+  name: string
+  otp1: string
+  otp2: string
+  otp3: string
+  otp4: string
+  registeredSince: number
+  sessionLifetime: number
+  yubikey: string
 }
