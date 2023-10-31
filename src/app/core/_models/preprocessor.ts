050a4abbf484103ef5eb6838e63ec52f78911649
itbf9 <itbf@proton.me>
2023-10-31 11:03:00 +0100

    Rename and update preprocessor interface

--- src/app/core/_models/preprocessor.ts
+++
@@ -1,9 +0,0 @@
-export interface Preprocessor {
-  preprocessorId: number
-  name: string;
-  url: string;
-  binaryName: string;
-  keyspaceCommand: string;
-  skipCommand: string;
-  limitCommand: string;
-}
