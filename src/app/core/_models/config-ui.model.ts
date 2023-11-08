f7c6ca544a63492dedd757984bf8c2bfab2257ea
itbf9 <itbf@proton.me>
2023-11-08 08:39:11 +0100

    Add agent table default settings

--- src/app/core/_models/config-ui.model.ts
+++ src/app/core/_models/config-ui.model.ts
@@ -1,3 +1,5 @@
+import { AgentsTableColumnLabel } from "../_components/tables/agents-table/agents-table.constants"
+
 export type Layout = 'full' | 'fixed'
 export type Theme = 'light' | 'dark'
 
@@ -18,7 +20,18 @@ export const uiConfigDefault: UIConfig = {
   layout: 'fixed',
   theme: 'light',
   timefmt: 'dd/MM/yyyy h:mm:ss',
-  tableSettings: {},
+  tableSettings: {
+    agentTable: [
+      AgentsTableColumnLabel.ID,
+      AgentsTableColumnLabel.STATUS,
+      AgentsTableColumnLabel.NAME,
+      AgentsTableColumnLabel.USER,
+      AgentsTableColumnLabel.CLIENT,
+      AgentsTableColumnLabel.GPUS_CPUS,
+      AgentsTableColumnLabel.LAST_ACTIVITY,
+      AgentsTableColumnLabel.ACCESS_GROUP
+    ],
+  },
   refreshPage: false,
   refreshInterval: 10
 }
