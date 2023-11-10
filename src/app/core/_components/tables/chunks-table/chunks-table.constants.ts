861d91bf73b820f602ed694efeef2bc9de2e316f
itbf9 <itbf@proton.me>
2023-11-10 10:42:48 +0100

    Add chunks table

---
+++ src/app/core/_components/tables/chunks-table/chunks-table.constants.ts
@@ -0,0 +1,14 @@
+export const ChunksTableColumnLabel = {
+  ID: 'ID',
+  START: 'Start',
+  LENGTH: 'Length',
+  CHECKPOINT: 'Checkpoint',
+  PROGRESS: 'Progress',
+  TASK: 'Task',
+  AGENT: 'Agent',
+  DISPATCH_TIME: 'Dispatch Time',
+  LAST_ACTIVITY: 'Last Activity',
+  TIME_SPENT: 'Time Spent',
+  STATE: 'State',
+  CRACKED: 'Cracked'
+};
