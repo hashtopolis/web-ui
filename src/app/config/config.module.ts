898571652a0a62db1154c0706bf63d8f90a9ff6c
Benito Díaz-Medina <benitodm90@gmail.com>
2023-11-13 13:34:31 +0000

    Config Module, remove unused modules/components

--- src/app/config/config.module.ts
+++ src/app/config/config.module.ts
@@ -7,45 +7,27 @@ import { ConfigRoutingModule } from './config-routing.module';
 import { CoreComponentsModule } from '../core/_components/core-components.module';
 import { CrackersComponent } from './engine/crackers/crackers.component';
 import { DataTablesModule } from 'angular-datatables';
-import { EditCrackersComponent } from './engine/crackers/edit-version/edit-crackers.component';
 import { EditHealthChecksComponent } from './health-checks/edit-health-check/edit-health-checks.component';
-import { EngineMenuComponent } from './engine/engine-menu';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HashtypeComponent } from './hashtypes/hashtype/hashtype.component';
 import { HashtypesComponent } from './hashtypes/hashtypes.component';
 import { HealthChecksComponent } from './health-checks/health-checks.component';
 import { LogComponent } from './log/log.component';
-import { NewAgentBinariesComponent } from './engine/agent-binaries/agent-binary/new-agent-binaries.component';
-import { NewCrackerComponent } from './engine/crackers/new-cracker/new-cracker.component';
-import { NewCrackersComponent } from './engine/crackers/new-version/new-crackers.component';
 import { NewHealthChecksComponent } from './health-checks/new-health-check/new-health-checks.component';
-import { NewPreprocessorComponent } from './engine/preprocessors/preprocessor/new-preprocessor.component';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PipesModule } from '../shared/pipes.module';
 import { PreprocessorsComponent } from './engine/preprocessors/preprocessors.component';
 import { RouterModule } from '@angular/router';
-import { ServerComponent } from './server/server.component';
-import { SettingsMenuComponent } from './server/settings-menu';
 
 @NgModule({
   declarations: [
-    NewAgentBinariesComponent,
     EditHealthChecksComponent,
-    NewPreprocessorComponent,
     NewHealthChecksComponent,
     PreprocessorsComponent,
     AgentBinariesComponent,
     HealthChecksComponent,
-    EditCrackersComponent,
-    SettingsMenuComponent,
-    NewCrackersComponent,
-    NewCrackerComponent,
-    EngineMenuComponent,
     HashtypesComponent,
     CrackersComponent,
-    HashtypeComponent,
-    ServerComponent,
     LogComponent
   ],
   imports: [
