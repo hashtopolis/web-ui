8659b532a3c2bdde712e1d23a4807f206a3f8f7a
Benito Díaz-Medina <benitodm90@gmail.com>
2023-11-20 10:27:27 +0000

    Error component

--- src/app/shared/alert/error/error.component.ts
+++ src/app/shared/alert/error/error.component.ts
@@ -1,9 +1,16 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-timeout',
-  templateUrl: './error.component.html'
+  templateUrl: './error.component.html',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogModule]
 })
 export class ErrorModalComponent {
   message: any;
