52ca45884e2c97335778ad67b92633ab3fc63c45
Benito Díaz-Medina <benitodm90@gmail.com>
2023-12-14 16:01:49 +0000

    Remove unused libraries

--- src/app/tasks/preconfigured-tasks/preconfigured-tasks.component.ts
+++ src/app/tasks/preconfigured-tasks/preconfigured-tasks.component.ts
@@ -1,26 +1,5 @@
-import {
-  faArchive,
-  faBookmark,
-  faCopy,
-  faEdit,
-  faFileExport,
-  faFileImport,
-  faHomeAlt,
-  faLock,
-  faPlus,
-  faTrash
-} from '@fortawesome/free-solid-svg-icons';
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { environment } from './../../../environments/environment';
-import { DataTableDirective } from 'angular-datatables';
-import { Subject, Subscription } from 'rxjs';
-
 import { AutoTitleService } from 'src/app/core/_services/shared/autotitle.service';
-import { AlertService } from 'src/app/core/_services/shared/alert.service';
-import { GlobalService } from 'src/app/core/_services/main.service';
-import { SERV } from '../../core/_services/main.config';
-
-declare let $: any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-preconfigured-tasks',
