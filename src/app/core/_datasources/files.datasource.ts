32a5a4c4dba702f540e09f8e5762dfe9d1be31a6
Benito Díaz-Medina <benitodm90@gmail.com>
2024-01-23 20:24:21 +0000

    Edit pretasks and tasks table

--- src/app/core/_datasources/files.datasource.ts
+++ src/app/core/_datasources/files.datasource.ts
@@ -1,12 +1,13 @@
-import { File, FileType } from '../_models/file.model';
-import { catchError, finalize, of } from 'rxjs';
-
+import { catchError, finalize, forkJoin, of } from 'rxjs';
 import { BaseDataSource } from './base.datasource';
 import { ListResponseWrapper } from '../_models/response.model';
 import { SERV } from '../_services/main.config';
+import { File, FileType } from '../_models/file.model';
 
 export class FilesDataSource extends BaseDataSource<File> {
   private fileType: FileType = 0;
+  private editIndex?: number;
+  private editType?: number;
 
   setFileType(fileType: FileType): void {
     this.fileType = fileType;
@@ -16,18 +17,37 @@ export class FilesDataSource extends BaseDataSource<File> {
     return this.fileType;
   }
 
+  setEditValues(index: number, editType: number): void {
+    this.editIndex = index;
+    this.editType = editType;
+  }
+
   loadAll(): void {
     this.loading = true;
 
     const startAt = this.currentPage * this.pageSize;
-    const params = {
-      maxResults: this.pageSize,
-      startAt: startAt,
-      expand: 'accessGroup',
-      filter: `fileType=${this.fileType}`
-    };
 
-    const files$ = this.service.getAll(SERV.FILES, params);
+    let files$;
+
+    if (this.editIndex !== undefined) {
+      if (this.editType === 0) {
+        files$ = this.service.get(SERV.TASKS, this.editIndex, {
+          expand: 'files'
+        });
+      } else {
+        files$ = this.service.get(SERV.PRETASKS, this.editIndex, {
+          expand: 'pretaskFiles'
+        });
+      }
+    } else {
+      const params = {
+        maxResults: this.pageSize,
+        startAt: startAt,
+        expand: 'accessGroup',
+        filter: `fileType=${this.fileType}`
+      };
+      files$ = this.service.getAll(SERV.FILES, params);
+    }
 
     this.subscriptions.push(
       files$
@@ -36,21 +56,43 @@ export class FilesDataSource extends BaseDataSource<File> {
           finalize(() => (this.loading = false))
         )
         .subscribe((response: ListResponseWrapper<File>) => {
-          const files: File[] = response.values;
+          let files: File[];
+
+          if (this.editType === 0) {
+            files = response['files'];
+            console.log(files);
+          } else if (this.editType === 1) {
+            files = response['pretaskFiles'];
+          } else {
+            files = response.values;
+          }
 
           files.map((file: File) => {
             if (file.accessGroup) {
               file.accessGroupId = file.accessGroup.accessGroupId;
               file.accessGroupName = file.accessGroup.groupName;
+            } else {
+              const accessGroups$ = this.service.get(
+                SERV.ACCESS_GROUPS,
+                file.accessGroupId
+              );
+              forkJoin(accessGroups$).subscribe(
+                (accessGroupResponses: any[]) => {
+                  file.accessGroupName = accessGroupResponses['GroupName'];
+                }
+              );
             }
           });
 
-          this.setPaginationConfig(
-            this.pageSize,
-            this.currentPage,
-            response.total
-          );
-          this.setData(response.values);
+          if (!this.editType) {
+            this.setPaginationConfig(
+              this.pageSize,
+              this.currentPage,
+              response.total
+            );
+          }
+
+          this.setData(files);
         })
     );
   }
