685bad225524f64e12d5776beb0013ae8938f1ff
Benito Díaz-Medina <benitodm90@gmail.com>
2024-05-17 10:06:48 +0100

    Login interceptor Doc and on wrong IP address display graceful

--- src/app/core/_interceptors/auth-interceptor.service.ts
+++ src/app/core/_interceptors/auth-interceptor.service.ts
@@ -1,13 +1,17 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import {
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { take, exhaustMap } from 'rxjs';
+import { exhaustMap, take } from 'rxjs';
 
 import { AuthService } from '../_services/access/auth.service';
 import { User, UserData } from '../_models/auth-user.model';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return this.authService.user.pipe(
       take(1),
@@ -15,15 +19,17 @@ export class AuthInterceptorService implements HttpInterceptor {
         if (!user) {
           return next.handle(req);
         }
-        const now = new Date().getTime()
-        const exp = new Date(user._expires).getTime()
+        const now = new Date().getTime();
+        const exp = new Date(user._expires).getTime();
         const modifiedReq = req.clone({
           setHeaders: {
-            Authorization: `Bearer ${user._expires && now < exp ? user._token : ''}`
+            Authorization: `Bearer ${
+              user._expires && now < exp ? user._token : ''
+            }`
           }
         });
         return next.handle(modifiedReq);
-      }));
+      })
+    );
   }
 }
-
