7b08a1cde0620707cc4dcb6c2db3b15ae5ab8804
gpascal123 <171025622+gpascal123@users.noreply.github.com>
2025-01-31 10:58:44 +0100

    The Files Pages now show the correct values again (#120)

--- src/app/core/_models/access-group.model.ts
+++ src/app/core/_models/access-group.model.ts
@@ -1,3 +1,5 @@
+import { RelationshipAttributes } from './hash.model';
+
 export interface AccessGroup {
   _id: number;
   _self: string;
@@ -5,4 +7,36 @@ export interface AccessGroup {
   groupName: string;
   userMembers?: [];
   agentMembers?: [];
+}
+
+
+
+export interface AccessGroupData {
+  type: string;
+  id: number;
+  attributes: AccessGroupDataAttributes;
+  links: AccessGroupDataLinks;
+  relationships: AccessGroupRelationships;
+}
+
+export interface AccessGroupDataAttributes {
+  groupName: string;
+}
+
+export interface AccessGroupDataLinks {
+  self: string;
+}
+
+export interface AccessGroupRelationships {
+  agentMembers: AccessGroupRelationshipAttributes;
+  userMembers: AccessGroupRelationshipAttributes;
+}
+
+export interface AccessGroupRelationshipAttributes {
+  links: AccessGroupAttributesLinks;
+}
+
+export interface AccessGroupAttributesLinks {
+  self: string;
+  related: string;
 }
