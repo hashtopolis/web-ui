6e85e6c666a673cf1349ff10b76a8bff4d0a1d30
gpascal123 <gpascal123@outlook.com>
2025-02-04 14:00:08 +0100

    The Preprocessors Page now shows the correct values again

--- src/app/core/_models/preprocessor.model.ts
+++ src/app/core/_models/preprocessor.model.ts
@@ -9,3 +9,25 @@ export interface Preprocessor {
   skipCommand: string;
   limitCommand: string;
 }
+
+
+
+export interface PreprocessorData {
+  type: string;
+  id: number;
+  attributes: PreprocessorAttributes;
+  links: PreprocessorDataLinks;
+}
+
+export interface PreprocessorAttributes {
+  name: string;
+  url: string;
+  binaryName: string;
+  keyspaceCommand: string;
+  skipCommand: string;
+  limitCommand: string;
+}
+
+export interface PreprocessorDataLinks {
+  self: string;
+}
