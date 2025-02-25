f3c04685cd5bbec6497069494d1ed6492e7a262e
Niklas <niklas.brynolf@gmail.com>
2025-02-25 12:56:23 +0000

    wip table

--- src/app/core/_components/tables/agent-view-table/agents-view-table.constants.ts
+++ src/app/core/_components/tables/agent-view-table/agents-view-table.constants.ts
@@ -1,4 +1,4 @@
-export enum AgentsViewTableCol {
+/* export enum AgentsViewTableCol {
   ID,
   NAME,
   TEMPERATURE,
@@ -12,4 +12,23 @@ export const AgentsViewTableColumnLabel = {
   [AgentsViewTableCol.DEVICE_UTILISATION]: 'Device Utilisation',
   [AgentsViewTableCol.TEMPERATURE]: 'Temperature',
   [AgentsViewTableCol.CPU_UTILISATION]: 'CPU Utilisation'
+}; */
+export enum AgentsViewTableCol {
+  ID,
+  STATUS,
+  NAME,
+  AGENT_STATUS,
+  WORKING_ON,
+  ASSIGNED,
+  LAST_ACTIVITY
+}
+
+export const AgentsViewTableColumnLabel = {
+  [AgentsViewTableCol.ID]: 'ID',
+  [AgentsViewTableCol.STATUS]: 'Status',
+  [AgentsViewTableCol.NAME]: 'Name',
+  [AgentsViewTableCol.AGENT_STATUS]: 'Agent Status',
+  [AgentsViewTableCol.WORKING_ON]: 'Currently working on',
+  [AgentsViewTableCol.ASSIGNED]: 'Assigned to',
+  [AgentsViewTableCol.LAST_ACTIVITY]: 'Last Activity'
 };
