8733c81ddfbab65133ac7c5d38bc7257088179eb
gpascal123 <171025622+gpascal123@users.noreply.github.com>
2025-03-21 13:04:54 +0100

    The Notifications page has now been modified for the JSON serializer (#167)

--- src/app/core/_datasources/notifications.datasource.ts
+++ src/app/core/_datasources/notifications.datasource.ts
@@ -1,12 +1,12 @@
 import { catchError, finalize, of } from 'rxjs';
 
 import { BaseDataSource } from './base.datasource';
-import { ListResponseWrapper } from '../_models/response.model';
-import { NotificationData } from '../_models/notification.model';
+import { ResponseWrapper } from '../_models/response.model';
+import { JNotification } from '../_models/notification.model';
 import { SERV } from '../_services/main.config';
 import { RequestParamBuilder } from '@src/app/core/_services/params/builder-implementation.service';
 
-export class NotificationsDataSource extends BaseDataSource<NotificationData> {
+export class NotificationsDataSource extends BaseDataSource<JNotification> {
   loadAll(): void {
     this.loading = true;
     const params = new RequestParamBuilder().addInitial(this).create();
@@ -18,13 +18,15 @@ export class NotificationsDataSource extends BaseDataSource<NotificationData> {
           catchError(() => of([])),
           finalize(() => (this.loading = false))
         )
-        .subscribe((response: ListResponseWrapper<NotificationData>) => {
-          const notifications: NotificationData[] = response.data;
+        .subscribe((response: ResponseWrapper) => {
+
+          const responseData = { data: response.data, included: response.included };
+          const notifications = this.serializer.deserialize<JNotification[]>(responseData);
 
           this.setPaginationConfig(
             this.pageSize,
             this.currentPage,
-            response.total
+            notifications.length
           );
           this.setData(notifications);
         })
