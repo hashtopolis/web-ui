0f362206b32ec3069fa77845ac7f90fb811a2529
cv5ch <176032962+cv5ch@users.noreply.github.com>
2025-03-25 16:40:29 +0100

    Refactor storage service to json:api serializer

--- src/app/core/_services/shared/storage.service.ts
+++ src/app/core/_services/shared/storage.service.ts
@@ -1,16 +1,19 @@
-import { environment } from '@src/environments/environment';
 import { Injectable } from '@angular/core';
-import { GlobalService } from 'src/app/core/_services/main.service';
-import { SERV } from '../../../core/_services/main.config';
-import { ListResponseWrapper } from '../../_models/response.model';
-import { ConfigsData } from '../../_models/configs.model';
-import { RequestParamBuilder } from '@src/app/core/_services/params/builder-implementation.service';
+
+import { environment } from '@src/environments/environment';
+
+import { JConfig } from '@models/configs.model';
+import { ResponseWrapper } from '@models/response.model';
+
+import { GlobalService } from '@services/main.service';
+import { JsonAPISerializer } from '@services/api/serializer-service';
+import { RequestParamBuilder } from '@services/params/builder-implementation.service';
+import { SERV } from '@services/main.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UIConfigService {
-
   defaultSettings = false;
   cachevar = [
     // {name:'timefmt'},
@@ -31,14 +34,12 @@ export class UIConfigService {
   cexprity: number = 72 * 60 * 60; // Hours*minutes*Seconds Default: 72 hours
   private maxResults = environment.config.prodApiMaxResults;
 
-  constructor(
-    private gs: GlobalService
-  ) {
-  }
+  constructor(private gs: GlobalService) {}
 
   public checkStorage() {
     const defaults = JSON.parse(localStorage.getItem('uis'));
-    if (defaults) {  //Change to !defaults
+    if (defaults) {
+      //Change to !defaults
       this.checkExpiry();
       return this.defaultSettings;
     } else if (!defaults) {
@@ -51,29 +52,36 @@ export class UIConfigService {
   public checkExpiry() {
     const timestamp = this.getUIsettings('_timestamp').value || 0;
     const expires = this.getUIsettings('_expiresin').value || 0;
-    if ((Date.now() - timestamp) < expires) {
+    if (Date.now() - timestamp < expires) {
       this.storeDefault();
     }
   }
 
   public storeDefault() {
     const params = new RequestParamBuilder().setPageSize(this.maxResults).create();
-    this.gs.getAll(SERV.CONFIGS, params).subscribe((result: ListResponseWrapper<ConfigsData>) => {
+    this.gs.getAll(SERV.CONFIGS, params).subscribe((response: ResponseWrapper) => {
+      const configs = new JsonAPISerializer().deserialize<JConfig[]>({
+        data: response.data,
+        included: response.included
+      });
       const post_data = [];
       this.cachevar.forEach((data) => {
         const name = data.name;
-        let value: any = result.data.find(obj => obj.attributes.item === data.name).attributes;
+        let value: any = configs.find((config) => config.item === data.name);
         value = { name: name, value: value.value };
         post_data.push(value);
       });
-      const timeinfo = [{ name: '_timestamp', value: Date.now() }, { name: '_expiresin', value: this.cexprity }];
+      const timeinfo = [
+        { name: '_timestamp', value: Date.now() },
+        { name: '_expiresin', value: this.cexprity }
+      ];
 
       localStorage.setItem('uis', JSON.stringify([].concat(post_data, timeinfo)));
     });
   }
 
   public onUpdatingCheck(name: any) {
-    if (this.cachevar.some(e => e.name === name)) {
+    if (this.cachevar.some((e) => e.name === name)) {
       this.storeDefault();
     }
   }
@@ -83,14 +91,6 @@ export class UIConfigService {
     if (!uiconfig) {
       return null;
     }
-    return uiconfig.find(o => o.name === name);
+    return uiconfig.find((o) => o.name === name);
   }
-
 }
-
-
-
-
-
-
-
