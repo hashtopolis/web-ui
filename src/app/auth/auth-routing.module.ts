5dc8a166f0c56ba03bfa8359db8a7b2bfea0d68a
jessevz <jessevanzutphen7@hotmail.com>
2025-03-25 17:30:19 +0100

    Added login guard to prevent going to login page when already logged in (#178)

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,28 @@
+import { MyRoute, RouteData } from '../core/_models/routes.model';
+import { RouterModule, Routes } from '@angular/router';
+import { IsAuth } from '../core/_guards/login.guard';
+import { NgModule } from '@angular/core';
+import { AuthComponent } from './auth.component';
+
+const routes: MyRoute[] = [
+  {
+    path: 'auth',
+    children: [
+    {
+        path: '',
+        component: AuthComponent,
+        data: {
+          kind: 'auth',
+          breadcrumb: ''
+        },
+        canActivate: [IsAuth]
+      },
+    ]
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule {}
