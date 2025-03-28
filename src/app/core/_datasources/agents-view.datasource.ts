7d34b17c6a57a8f2c6cab1cbc025c1c1035c95a7
Niklas <niklas.brynolf@gmail.com>
2025-03-28 12:10:47 +0000

    wip jsonser

--- src/app/core/_datasources/agents-view.datasource.ts
+++ src/app/core/_datasources/agents-view.datasource.ts
@@ -1,10 +1,11 @@
-import { Agent, AgentStats } from '../_models/agent.model';
+import { Agent, AgentStats, JAgent } from '../_models/agent.model';
 import { Chunk, ChunkData } from '../_models/chunk.model';
+import { ListResponseWrapper, ResponseWrapper } from '../_models/response.model';
 import { catchError, finalize, firstValueFrom, forkJoin, of } from 'rxjs';
 
 import { ASC } from '../_constants/agentsc.config';
 import { BaseDataSource } from './base.datasource';
-import { ListResponseWrapper } from '../_models/response.model';
+import { JAgentStat } from '../_models/agent-stats.model';
 import { RequestParamBuilder } from '../_services/params/builder-implementation.service';
 import { RequestParams } from '../_models/request-params.model';
 import { SERV } from '../_services/main.config';
@@ -31,8 +32,49 @@ export class AgentsViewDataSource extends BaseDataSource<Agent> {
     if (this._agentId) {
       // params.filter = `chunkId=${this._agentId}`;
     }
+    const params = new RequestParamBuilder().setPageSize(this.maxResults).create();
+    /*     this.service.getAll(SERV.AGENTS, agentParams).subscribe((agentStatsResponse: ResponseWrapper) => {
+      console.log('BEFORE ', agentStatsResponse);
+      const agentStats = this.serializer.deserialize<JAgentStat[]>(agentStatsResponse);
+      console.log('AFTER ', agentStats);
+      const tempDateFilter = agentStats.filter((agentStat) => agentStat.time > 10000000); // Temp
+    }); */
 
     const agents$ = this.service.getAll(SERV.AGENTS, agentParams);
+    const agents2$ = this.service.getAll(SERV.AGENTS, agentParams);
+    agents2$
+      .pipe(
+        catchError(() => of([])),
+        finalize(() => (this.loading = false))
+      )
+      .subscribe((agentStatsResponse: ResponseWrapper) => {
+        const agent = this.serializer.deserialize<JAgent[]>(agentStatsResponse);
+        console.log('AFTER ', agent);
+        agent.map((agent: JAgent) => {
+          const tempDateFilter = agent.agentStats.filter((u) => u.time > 10000000);
+          const statTemp = tempDateFilter.filter((u) => u.statType == ASC.GPU_TEMP);
+          const statDevice = tempDateFilter.filter((u) => u.statType == ASC.GPU_UTIL);
+          const statCpu = tempDateFilter.filter((u) => u.statType == ASC.CPU_UTIL);
+
+          /* agent.maxTemp = */ const maxTemp = Math.round(
+            statTemp.reduce((prev, current) => (prev.value > current.value ? prev : current)).value[0]
+          );
+
+          /* agent.avgCpu = */ const avgCpu = Math.round(
+            statCpu.reduce((sum, current) => sum + current.value.reduce((a, b) => a + b, 0), 0) / statCpu.length
+          );
+
+          /* agent.avgDevice = */ const avgDevice = Math.round(
+            statDevice.reduce((sum, current) => sum + current.value.reduce((a, b) => a + b, 0), 0) / statDevice.length
+          );
+          console.log('maxTemp ', maxTemp);
+          console.log('avgCpu ', avgCpu);
+          console.log('avgDevice ', avgDevice);
+        });
+        /*         agentStats.map((agentStat) => {
+          console.log('AGENT STATS ', agentStat);
+        }); */
+      });
 
     agents$
       .pipe(
@@ -41,7 +83,6 @@ export class AgentsViewDataSource extends BaseDataSource<Agent> {
       )
       .subscribe((a: ListResponseWrapper<Agent>) => {
         const agents: Agent[] = a.values;
-  
 
         agents.map((agent: Agent) => {
           const tempDateFilter = agent.agentstats.filter((u) => u.time > 10000000);
