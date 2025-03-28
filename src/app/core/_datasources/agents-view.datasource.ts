c6c217dbdcfd7ea2bb843149cbdfe839c13a8bd0
Niklas <niklas.brynolf@gmail.com>
2025-03-28 08:51:42 +0000

    data soure fix

--- src/app/core/_datasources/agents-view.datasource.ts
+++ src/app/core/_datasources/agents-view.datasource.ts
@@ -5,6 +5,7 @@ import { catchError, finalize, firstValueFrom, forkJoin, of } from 'rxjs';
 import { ASC } from '../_constants/agentsc.config';
 import { BaseDataSource } from './base.datasource';
 import { ListResponseWrapper } from '../_models/response.model';
+import { RequestParamBuilder } from '../_services/params/builder-implementation.service';
 import { RequestParams } from '../_models/request-params.model';
 import { SERV } from '../_services/main.config';
 
@@ -20,18 +21,22 @@ export class AgentsViewDataSource extends BaseDataSource<Agent> {
 
     const startAt = this.currentPage * this.pageSize;
     const sorting = this.sortingColumn;
-
-    const agentParams: RequestParams = {
+    const agentParams2 = new RequestParamBuilder()
+      .addInitial(this)
+      .addInclude('accessGroups')
+      .addInclude('agentStats')
+      .create();
+    /*     const agentParams: RequestParams = {
       maxResults: this.pageSize,
       startsAt: startAt,
       expand: 'agentstats'
-    };
+    }; */
 
     if (this._agentId) {
       // params.filter = `chunkId=${this._agentId}`;
     }
 
-    const agents$ = this.service.getAll(SERV.AGENTS, agentParams);
+    const agents$ = this.service.getAll(SERV.AGENTS, agentParams2);
 
     agents$
       .pipe(
@@ -42,37 +47,21 @@ export class AgentsViewDataSource extends BaseDataSource<Agent> {
         const agents: Agent[] = a.values;
 
         agents.map((agent: Agent) => {
-          const tempDateFilter = agent.agentstats.filter(
-            (u) => u.time > 10000000
-          );
-          const statTemp = tempDateFilter.filter(
-            (u) => u.statType == ASC.GPU_TEMP
-          );
-          const statDevice = tempDateFilter.filter(
-            (u) => u.statType == ASC.GPU_UTIL
-          );
-          const statCpu = tempDateFilter.filter(
-            (u) => u.statType == ASC.CPU_UTIL
-          );
+          const tempDateFilter = agent.agentstats.filter((u) => u.time > 10000000);
+          const statTemp = tempDateFilter.filter((u) => u.statType == ASC.GPU_TEMP);
+          const statDevice = tempDateFilter.filter((u) => u.statType == ASC.GPU_UTIL);
+          const statCpu = tempDateFilter.filter((u) => u.statType == ASC.CPU_UTIL);
 
           agent.maxTemp = Math.round(
-            statTemp.reduce((prev, current) =>
-              prev.value > current.value ? prev : current
-            ).value[0]
+            statTemp.reduce((prev, current) => (prev.value > current.value ? prev : current)).value[0]
           );
 
           agent.avgCpu = Math.round(
-            statCpu.reduce(
-              (sum, current) => sum + current.value.reduce((a, b) => a + b, 0),
-              0
-            ) / statCpu.length
+            statCpu.reduce((sum, current) => sum + current.value.reduce((a, b) => a + b, 0), 0) / statCpu.length
           );
 
           agent.avgDevice = Math.round(
-            statDevice.reduce(
-              (sum, current) => sum + current.value.reduce((a, b) => a + b, 0),
-              0
-            ) / statDevice.length
+            statDevice.reduce((sum, current) => sum + current.value.reduce((a, b) => a + b, 0), 0) / statDevice.length
           );
         });
         this.setPaginationConfig(this.pageSize, this.currentPage, a.total);
