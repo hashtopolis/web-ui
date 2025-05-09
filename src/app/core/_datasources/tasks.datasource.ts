05b7d43ba48fd520a45c90a744e2c1e626c2720e
gluafamichl <86108041+gluafamichl@users.noreply.github.com>
2025-05-09 08:56:17 +0200

    1269 enhancement reduce chunk related backend calls (#232)
    * Mod: reduce backend calls to retrieve chunk data
    
    * Mod: reduce backend calls to retrieve chunk data
    
    * Mod: filter chunkdata by visible agents to reduce backend load
    
    ---------
    
    Co-authored-by: gluafamichl <>

--- src/app/core/_datasources/tasks.datasource.ts
+++ src/app/core/_datasources/tasks.datasource.ts
@@ -1,6 +1,7 @@
 import { catchError, of } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 
+import { JChunk } from '@models/chunk.model';
 import { FilterType } from '@models/request-params.model';
 import { ResponseWrapper } from '@models/response.model';
 import { JTaskWrapper } from '@models/task-wrapper.model';
@@ -13,14 +14,9 @@ import { BaseDataSource } from '@datasources/base.datasource';
 
 export class TasksDataSource extends BaseDataSource<JTaskWrapper> {
   private _isArchived = false;
-  private _hashlistId = 0;
 
   setIsArchived(isArchived: boolean): void {
     this._isArchived = isArchived;
-  }
-
-  setHashlistId(hashlistId: number): void {
-    this._hashlistId = hashlistId;
   }
 
   loadAll(): void {
@@ -45,17 +41,33 @@ export class TasksDataSource extends BaseDataSource<JTaskWrapper> {
           catchError(() => of([])),
           finalize(() => (this.loading = false))
         )
-        .subscribe(async (response: ResponseWrapper) => {
+        .subscribe((response: ResponseWrapper) => {
           const taskWrappers = this.serializer.deserialize<JTaskWrapper[]>({
             data: response.data,
             included: response.included
           });
           this.setPaginationConfig(this.pageSize, this.currentPage, taskWrappers.length);
-          for (const taskWrapper of taskWrappers) {
-            const task: JTask = taskWrapper.tasks[0];
-            taskWrapper.chunkData = await this.getChunkData(task.id, false, task.keyspace);
-          }
-          this.setData(taskWrappers);
+          const chunkParams = new RequestParamBuilder().addFilter({
+            field: 'taskId',
+            operator: FilterType.IN,
+            value: taskWrappers.map((wrapper) => wrapper.tasks[0].id)
+          });
+
+          this.subscriptions.push(
+            this.service
+              .getAll(SERV.CHUNKS, chunkParams.create())
+              .pipe(finalize(() => this.setData(taskWrappers)))
+              .subscribe((chunkResponse: ResponseWrapper) => {
+                const chunks = this.serializer.deserialize<JChunk[]>({
+                  data: chunkResponse.data,
+                  included: chunkResponse.included
+                });
+                taskWrappers.forEach((taskWrapper) => {
+                  const task: JTask = taskWrapper.tasks[0];
+                  taskWrapper.chunkData = this.convertChunks(task.id, chunks, false, task.keyspace);
+                });
+              })
+          );
         })
     );
   }
