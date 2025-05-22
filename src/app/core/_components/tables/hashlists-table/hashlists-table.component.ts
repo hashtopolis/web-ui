3414ceeabf1ea3272735a4deff5aa4235f47f247
gluafamichl <86108041+gluafamichl@users.noreply.github.com>
2025-05-22 15:18:39 +0200

    1244-bug-search-hashes-throws-typeerror (#251)
    * Fix: type error due to initialisation order of event handler
    
    * Fix: type error due to initialisation order of event handler
    
    ---------
    
    Co-authored-by: gluafamichl <>

--- src/app/core/_components/tables/hashlists-table/hashlists-table.component.ts
+++ src/app/core/_components/tables/hashlists-table/hashlists-table.component.ts
@@ -1,4 +1,4 @@
-import { Observable, catchError, forkJoin, of } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
@@ -237,9 +237,9 @@ export class HashlistsTableComponent extends BaseTableComponent implements OnIni
   private bulkActionArchive(hashlists: JHashlist[], isArchived: boolean): void {
     const action = isArchived ? 'archived' : 'unarchived';
     this.subscriptions.push(
-      this.gs.bulkUpdate(SERV.HASHLISTS, hashlists, {isArchived: isArchived}).subscribe((results) => {
-          this.snackBar.open(`Successfully ${action} hashlists!`, 'Close');
-          this.reload();
+      this.gs.bulkUpdate(SERV.HASHLISTS, hashlists, { isArchived: isArchived }).subscribe(() => {
+        this.snackBar.open(`Successfully ${action} hashlists!`, 'Close');
+        this.reload();
       })
     );
   }
@@ -249,15 +249,18 @@ export class HashlistsTableComponent extends BaseTableComponent implements OnIni
    */
   private bulkActionDelete(hashlists: JHashlist[]): void {
     this.subscriptions.push(
-      this.gs.bulkDelete(SERV.HASHLISTS, hashlists).pipe(
-        catchError((error) => {
-          console.error('Error during deletion: ', error);
-          return [];
+      this.gs
+        .bulkDelete(SERV.HASHLISTS, hashlists)
+        .pipe(
+          catchError((error) => {
+            console.error('Error during deletion: ', error);
+            return [];
+          })
+        )
+        .subscribe(() => {
+          this.snackBar.open(`Successfully deleted hashlists!`, 'Close');
+          this.dataSource.reload();
         })
-      ).subscribe((results) => {
-        this.snackBar.open(`Successfully deleted hashlists!`, 'Close');
-        this.dataSource.reload();
-      })
     );
   }
 
@@ -323,7 +326,7 @@ export class HashlistsTableComponent extends BaseTableComponent implements OnIni
   }
 
   /**
-   * Show hashcounbt and render hashlist link
+   * Show hashcount and render hashlist link
    * @param hashlist - hashlist object to show count for
    * @return observable array containing the link to render
    * @private
@@ -333,7 +336,7 @@ export class HashlistsTableComponent extends BaseTableComponent implements OnIni
     if (hashlist) {
       links.push({
         routerLink: ['/hashlists', 'hashes', 'hashlists', hashlist.id],
-        label: hashlist.hashCount
+        label: hashlist.hashCount.toLocaleString()
       });
     }
     return of(links);
