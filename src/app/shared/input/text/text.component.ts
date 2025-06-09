160367ab9633b8ef27e8db79cd6197b1a1eb4897
Niklas <niklas.brynolf@gmail.com>
2025-06-09 09:04:38 +0000

    show password wip

--- src/app/shared/input/text/text.component.ts
+++ src/app/shared/input/text/text.component.ts
@@ -1,7 +1,8 @@
-import { coerceBooleanProperty } from '@angular/cdk/coercion';
-import { AbstractInputComponent } from '../abstract-input';
 import { Component, Input, forwardRef } from '@angular/core';
+
+import { AbstractInputComponent } from '../abstract-input';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
 
 /**
  * Custom Input Text Component.
@@ -18,21 +19,23 @@ import { NG_VALUE_ACCESSOR } from '@angular/forms';
  * ```
  */
 @Component({
-    selector: 'input-text',
-    templateUrl: './text.component.html',
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => InputTextComponent),
-            multi: true
-        }
-    ],
-    standalone: false
+  selector: 'input-text',
+  templateUrl: './text.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputTextComponent),
+      multi: true
+    }
+  ],
+  standalone: false
 })
 export class InputTextComponent extends AbstractInputComponent<string> {
   @Input() pattern: string | RegExp;
   @Input() inputType: 'text' | 'password' = 'text';
   @Input() icon: string;
+  @Input() showPasswordToggle: boolean = false;
+  isVisible = true;
 
   constructor() {
     super();
