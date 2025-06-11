9158cfb501d736a5e92fdbf09edd10f2fd6b53f9
cv5ch <176032962+cv5ch@users.noreply.github.com>
2025-06-11 11:53:05 +0200

    Add new form component for creating a new cracker type

--- src/app/shared/input/text/text.component.ts
+++ src/app/shared/input/text/text.component.ts
@@ -1,7 +1,7 @@
-import { coerceBooleanProperty } from '@angular/cdk/coercion';
-import { AbstractInputComponent } from '../abstract-input';
 import { Component, Input, forwardRef } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import { AbstractInputComponent } from '@src/app/shared/input/abstract-input';
 
 /**
  * Custom Input Text Component.
@@ -18,21 +18,22 @@ import { NG_VALUE_ACCESSOR } from '@angular/forms';
  * ```
  */
 @Component({
-    selector: 'input-text',
-    templateUrl: './text.component.html',
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => InputTextComponent),
-            multi: true
-        }
-    ],
-    standalone: false
+  selector: 'input-text',
+  templateUrl: './text.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputTextComponent),
+      multi: true
+    }
+  ],
+  standalone: false
 })
 export class InputTextComponent extends AbstractInputComponent<string> {
   @Input() pattern: string | RegExp;
   @Input() inputType: 'text' | 'password' = 'text';
   @Input() icon: string;
+  @Input() width: string = '';
 
   constructor() {
     super();
