b9131bbc23fcf4a7c802b943afd2c9e7131b4166
Niklas <niklas.brynolf@gmail.com>
2025-06-18 11:22:45 +0000

    wip error table

---
+++ src/app/core/_components/tables/agent-error-table/agent-error-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-error-table',
+  imports: [],
+  templateUrl: './agent-error-table.component.html',
+  styleUrl: './agent-error-table.component.scss'
+})
+export class AgentErrorTableComponent {
+
+}
