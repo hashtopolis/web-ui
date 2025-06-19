cc20ffc1bd018055cbb93780506ffd173a675eff
Niklas <niklas.brynolf@gmail.com>
2025-06-19 08:30:40 +0000

    agetn error model

---
+++ src/app/core/_models/agent-errors.model.ts
@@ -0,0 +1,12 @@
+import { BaseModel } from "./base.model";
+import { NumberSymbol } from "@angular/common";
+
+export interface JAgentErrors extends BaseModel {
+    agentId: number
+    chunkId: number
+    error: string
+    id: number
+    taskId: number
+    time: NumberSymbol
+    type: string
+}
