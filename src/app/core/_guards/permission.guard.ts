cf71175afeb22b8558c101cc5da024f0cc475c1a
cv5ch <176032962+cv5ch@users.noreply.github.com>
2025-06-26 15:47:26 +0200

    First draft for permissionService (#305)

--- src/app/core/_guards/permission.guard.ts
+++ src/app/core/_guards/permission.guard.ts
@@ -1,17 +1,12 @@
-import { Observable, map, take } from 'rxjs';
-import { GlobalService } from 'src/app/core/_services/main.service';
+import { Observable, map } from 'rxjs';
 
 import { Injectable, inject } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivateFn } from '@angular/router';
 
-import { JGlobalPermissionGroup } from '@models/global-permission-group.model';
-
-import { SERV } from '@services/main.config';
+import { PermissionService } from '@services/permission/permission.service';
 import { AlertService } from '@services/shared/alert.service';
 
-import { Perm } from '@src/app/core/_constants/userpermissions.config';
-import { ResponseWrapper } from '@src/app/core/_models/response.model';
-import { JsonAPISerializer } from '@src/app/core/_services/api/serializer-service';
+import { PermissionResource } from '@src/app/core/_constants/userpermissions.config';
 
 @Injectable({
   providedIn: 'root'
@@ -19,21 +14,18 @@ import { JsonAPISerializer } from '@src/app/core/_services/api/serializer-servic
 export class PermissionGuard {
   constructor(
     private alert: AlertService,
-    private gs: GlobalService,
-    private serializer: JsonAPISerializer
+    private permissionService: PermissionService
   ) {}
 
   canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
-    return this.gs.ghelper(SERV.HELPER, 'getUserPermission').pipe(
-      take(1),
-      map((response: ResponseWrapper) => {
-        const responseData = { data: response.data, included: response.included };
-        const permissions = this.serializer.deserialize<JGlobalPermissionGroup>(responseData).permissions;
-        const permName = Perm[route.data['permission']].READ; //Get permission name
-        const hasAccess = permissions[permName]; //returns true or false
-        if (hasAccess || typeof hasAccess == 'undefined') {
+    const group = route.data['permission'] as PermissionResource;
+
+    return this.permissionService.hasPermission(group, 'READ').pipe(
+      map((hasAccess) => {
+        if (hasAccess) {
           return true;
         }
+
         this.alert.showErrorMessage('Access denied, please contact your Administrator.');
         return false;
       })
