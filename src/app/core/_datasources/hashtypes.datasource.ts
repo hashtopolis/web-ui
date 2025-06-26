6cb6af50c0d0b38561480aed97fd53cd7cafd71f
Niklas <niklas.brynolf@gmail.com>
2025-06-26 09:11:18 +0000

    wip backend filtering

--- src/app/core/_datasources/hashtypes.datasource.ts
+++ src/app/core/_datasources/hashtypes.datasource.ts
@@ -1,17 +1,34 @@
 import { catchError, finalize, of } from 'rxjs';
 
 import { BaseDataSource } from './base.datasource';
+import { FilterType } from '../_models/request-params.model';
 import { JHashtype } from '../_models/hashtype.model';
+import { RequestParamBuilder } from '@src/app/core/_services/params/builder-implementation.service';
 import { ResponseWrapper } from '../_models/response.model';
 import { SERV } from '../_services/main.config';
-import { RequestParamBuilder } from '@src/app/core/_services/params/builder-implementation.service';
 
 export class HashtypesDataSource extends BaseDataSource<JHashtype> {
   loadAll(): void {
     this.loading = true;
     const params = new RequestParamBuilder().addInitial(this).create();
+    const params2 = new RequestParamBuilder().addInitial(this)
+      .addFilter({ field: 'description', operator: FilterType.CONTAINS, value: 'RIPE' })
+      .create();
     const hashtypes$ = this.service.getAll(SERV.HASHTYPES, params);
-    this.subscriptions.push(
+    const test = this.service.getAll(SERV.HASHTYPES, params2);
+/*     this.subscriptions.push(
+      test
+        .pipe(
+          catchError(() => of([])),
+          finalize(() => (this.loading = false))
+        )
+        .subscribe((response: ResponseWrapper) => {
+          const hashtypes = this.serializer.deserialize<JHashtype[]>(response);
+          console.log('Test response:', response);
+          this.setData(hashtypes);
+        })
+    ); */
+        this.subscriptions.push(
       hashtypes$
         .pipe(
           catchError(() => of([])),
