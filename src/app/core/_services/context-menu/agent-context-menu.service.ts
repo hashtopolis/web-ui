1f434f9955652ea795b72e5faca404292db82764
gluafamichl <>
2025-06-27 08:21:10 +0200

    Add: add permission checks fpr agent's context menu

--- src/app/core/_services/context-menu/agent-context-menu.service.ts
+++ src/app/core/_services/context-menu/agent-context-menu.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { ContextMenuService } from '@services/context-menu/context-menu.service';
+import { PermissionService } from '@services/permission/permission.service';
 
 import { RowActionMenuLabel } from '@components/menus/row-action-menu/row-action-menu.constants';
 
@@ -8,16 +9,28 @@ import { RowActionMenuLabel } from '@components/menus/row-action-menu/row-action
   providedIn: 'root'
 })
 export class AgentContextMenuService {
-  constructor(private contextMenuService: ContextMenuService) {}
+  constructor(
+    private contextMenuService: ContextMenuService,
+    private permissionService: PermissionService
+  ) {}
 
   /**
    * Add context menu for all agent based tables
    */
   addAgentContextMenu(): void {
-    this.contextMenuService.addEditItem(RowActionMenuLabel.EDIT_AGENT);
-    this.contextMenuService.addDeactivateMenuItem(RowActionMenuLabel.DEACTIVATE_AGENT, 'isActive', true);
-    this.contextMenuService.addActivateMenuItem(RowActionMenuLabel.ACTIVATE_AGENT, 'isActive', false);
-    this.contextMenuService.addDeleteMenuItem(RowActionMenuLabel.UNASSIGN_AGENT, 'assignmentId', true);
-    this.contextMenuService.addDeleteMenuItem(RowActionMenuLabel.DELETE_AGENT, 'assignmentId', false);
+    this.permissionService.hasPermission('Agent', 'UPDATE').subscribe((response) => {
+      if (response) {
+        this.contextMenuService.addEditItem(RowActionMenuLabel.EDIT_AGENT);
+        this.contextMenuService.addDeactivateMenuItem(RowActionMenuLabel.DEACTIVATE_AGENT, 'isActive', true);
+        this.contextMenuService.addActivateMenuItem(RowActionMenuLabel.ACTIVATE_AGENT, 'isActive', false);
+        this.contextMenuService.addDeleteMenuItem(RowActionMenuLabel.UNASSIGN_AGENT, 'assignmentId', true);
+      }
+    });
+
+    this.permissionService.hasPermission('Agent', 'DELETE').subscribe((response) => {
+      if (response) {
+        this.contextMenuService.addDeleteMenuItem(RowActionMenuLabel.DELETE_AGENT, 'assignmentId', false);
+      }
+    });
   }
 }
