5d48821d40eab8b0a93b8f553589a7ce9ece9718
gluafamichl <>
2025-06-30 14:30:54 +0200

    Mod: delegate permission checks to base class

--- src/app/core/_services/context-menu/task-menu.service.ts
+++ src/app/core/_services/context-menu/task-menu.service.ts
@@ -1,7 +1,5 @@
 import { ContextMenuService } from '@services/context-menu/base/context-menu.service';
-import { PermissionCheck, PermissionService } from '@services/permission/permission.service';
-
-import { RowActionMenuLabel } from '@components/menus/row-action-menu/row-action-menu.constants';
+import { PermissionService } from '@services/permission/permission.service';
 
 export class TaskContextMenuService extends ContextMenuService {
   constructor(private permissionService: PermissionService) {
