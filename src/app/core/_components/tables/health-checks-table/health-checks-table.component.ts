08bc14e28a766438d43073e0d54194c2f8678568
Niklas <niklas.brynolf@gmail.com>
2025-07-10 10:35:35 +0000

    health check filter

--- src/app/core/_components/tables/health-checks-table/health-checks-table.component.ts
+++ src/app/core/_components/tables/health-checks-table/health-checks-table.component.ts
@@ -1,28 +1,24 @@
-/* eslint-disable @angular-eslint/component-selector */
-import { Observable, catchError, of } from 'rxjs';
-
 import { Component, OnDestroy, OnInit } from '@angular/core';
-
-import { JHealthCheck } from '@models/health-check.model';
-
-import { HealthCheckContextMenuService } from '@services/context-menu/config/health-check-menu.service';
-import { SERV } from '@services/main.config';
-
-import { ActionMenuEvent } from '@components/menus/action-menu/action-menu.model';
-import { BulkActionMenuAction } from '@components/menus/bulk-action-menu/bulk-action-menu.constants';
-import { RowActionMenuAction } from '@components/menus/row-action-menu/row-action-menu.constants';
-import { BaseTableComponent } from '@components/tables/base-table/base-table.component';
+import { HTTableColumn, HTTableRouterLink } from '@components/tables/ht-table/ht-table.models';
 import {
   HealthChecksTableCol,
   HealthChecksTableColumnLabel,
   HealthChecksTableStatusLabel
 } from '@components/tables/health-checks-table/health-checks-table.constants';
-import { HTTableColumn, HTTableRouterLink } from '@components/tables/ht-table/ht-table.models';
-import { TableDialogComponent } from '@components/tables/table-dialog/table-dialog.component';
+/* eslint-disable @angular-eslint/component-selector */
+import { Observable, catchError, of } from 'rxjs';
+
+import { ActionMenuEvent } from '@components/menus/action-menu/action-menu.model';
+import { BaseTableComponent } from '@components/tables/base-table/base-table.component';
+import { BulkActionMenuAction } from '@components/menus/bulk-action-menu/bulk-action-menu.constants';
 import { DialogData } from '@components/tables/table-dialog/table-dialog.model';
-
+import { FilterType } from '@src/app/core/_models/request-params.model';
+import { HealthCheckContextMenuService } from '@services/context-menu/config/health-check-menu.service';
 import { HealthChecksDataSource } from '@datasources/health-checks.datasource';
-
+import { JHealthCheck } from '@models/health-check.model';
+import { RowActionMenuAction } from '@components/menus/row-action-menu/row-action-menu.constants';
+import { SERV } from '@services/main.config';
+import { TableDialogComponent } from '@components/tables/table-dialog/table-dialog.component';
 import { formatUnixTimestamp } from '@src/app/shared/utils/datetime';
 
 @Component({
@@ -50,33 +46,20 @@ export class HealthChecksTableComponent extends BaseTableComponent implements On
     }
   }
 
-  filter(item: JHealthCheck, filterValue: string): boolean {
-    filterValue = filterValue.toLowerCase();
+  filter(input: string) {
     const selectedColumn = this.selectedFilterColumn;
-    // Filter based on selected column
-    switch (selectedColumn) {
-      case 'all': {
-        // Search across multiple relevant fields
-        return (
-          item.id.toString().includes(filterValue) || item.hashTypeDescription?.toLowerCase().includes(filterValue)
-        );
-      }
-      case 'id': {
-        return item.id.toString().includes(filterValue);
-      }
-      case 'hashtypeDescription': {
-        return item.hashTypeDescription?.toLowerCase().includes(filterValue);
-      }
-      default:
-        // Default fallback to task name
-        return item.hashTypeDescription?.toLowerCase().includes(filterValue);
+    if (input && input.length > 0) {
+      this.dataSource.loadAll({ value: input, field: selectedColumn, operator: FilterType.ICONTAINS });
+      return;
+    } else {
+      this.dataSource.loadAll(); // Reload all data if input is empty
     }
   }
   getColumns(): HTTableColumn[] {
     return [
       {
         id: HealthChecksTableCol.ID,
-        dataKey: 'id',
+        dataKey: '_id',
         routerLink: (healthCheck: JHealthCheck) => this.renderHealthCheckLink(healthCheck),
         isSortable: true,
         isSearchable: true,
@@ -94,8 +77,7 @@ export class HealthChecksTableComponent extends BaseTableComponent implements On
         dataKey: 'hashtypeDescription',
         render: (healthCheck: JHealthCheck) =>
           healthCheck.hashType ? `Brute Force (${healthCheck.hashType.description})` : '',
-        isSortable: true,
-        isSearchable: true,
+        /* isSortable: true, */ // Sorting is not implemented for this column needs to able to sort on included obejcts in teh query
         export: async (healthCheck: JHealthCheck) =>
           healthCheck.hashType ? `Brute Force (${healthCheck.hashType.description})` : ''
       },
--- src/app/core/_datasources/health-checks.datasource.ts
+++ src/app/core/_datasources/health-checks.datasource.ts
@@ -1,18 +1,20 @@
 import { catchError, finalize, forkJoin, of } from 'rxjs';
 
+import { BaseDataSource } from '@datasources/base.datasource';
+import { Filter } from '@models/request-params.model';
 import { JHealthCheck } from '@models/health-check.model';
-import { ResponseWrapper } from '@models/response.model';
-
 import { RequestParamBuilder } from '@services/params/builder-implementation.service';
+import { ResponseWrapper } from '@models/response.model';
 import { SERV } from '@services/main.config';
 
-import { BaseDataSource } from '@datasources/base.datasource';
-
 export class HealthChecksDataSource extends BaseDataSource<JHealthCheck> {
-  loadAll(): void {
+  loadAll(query?: Filter): void {
     this.loading = true;
-    const params = new RequestParamBuilder().addInitial(this).addInclude('hashType').create();
-    const healthChecks$ = this.service.getAll(SERV.HEALTH_CHECKS, params);
+    const params = new RequestParamBuilder().addInitial(this).addInclude('hashType');
+    if (query) {
+      params.addFilter(query);
+    }
+    const healthChecks$ = this.service.getAll(SERV.HEALTH_CHECKS, params.create());
 
     this.subscriptions.push(
       forkJoin([healthChecks$])
@@ -30,13 +32,7 @@ export class HealthChecksDataSource extends BaseDataSource<JHealthCheck> {
 
           const length = response.meta.page.total_elements;
 
-          this.setPaginationConfig(
-            this.pageSize,
-            length,
-            this.pageAfter,
-            this.pageBefore,
-            this.index
-          );
+          this.setPaginationConfig(this.pageSize, length, this.pageAfter, this.pageBefore, this.index);
           this.setData(healthChecks);
         })
     );
