05490d78ddad972057b65bb9f7f8c7d380974e40
Iqo <niklas.brynolf@gmail.com>
2025-07-15 11:13:47 +0200

    directive for disabling create user button (#340)

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,24 +1,23 @@
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DataTablesModule } from 'angular-datatables';
-
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
-import { CoreComponentsModule } from '@components/core-components.module';
-
-import { ComponentsModule } from '@src/app/shared/components.module';
-import { CoreFormsModule } from '@src/app/shared/forms.module';
-import { PipesModule } from '@src/app/shared/pipes.module';
 import { AllUsersComponent } from '@src/app/users/all-users/all-users.component';
+import { CommonModule } from '@angular/common';
+import { ComponentsModule } from '@src/app/shared/components.module';
+import { CoreComponentsModule } from '@components/core-components.module';
+import { CoreFormsModule } from '@src/app/shared/forms.module';
+import { DataTablesModule } from 'angular-datatables';
+import { DirectivesModule } from '@src/app/shared/directives.module';
+import { EditGlobalpermissionsgroupsComponent } from '@src/app/users/globalpermissionsgroups/edit-globalpermissionsgroups/edit-globalpermissionsgroups.component';
 import { EditGroupsComponent } from '@src/app/users/edit-groups/edit-groups.component';
 import { EditUsersComponent } from '@src/app/users/edit-users/edit-users.component';
-import { EditGlobalpermissionsgroupsComponent } from '@src/app/users/globalpermissionsgroups/edit-globalpermissionsgroups/edit-globalpermissionsgroups.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { GlobalpermissionsgroupsComponent } from '@src/app/users/globalpermissionsgroups/globalpermissionsgroups.component';
 import { GroupsComponent } from '@src/app/users/groups/groups.component';
 import { NewUserComponent } from '@src/app/users/new-user/new-user.component';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PipesModule } from '@src/app/shared/pipes.module';
+import { RouterModule } from '@angular/router';
 import { UsersRoutingModule } from '@src/app/users/users-routing.module';
 
 @NgModule({
@@ -38,6 +37,7 @@ import { UsersRoutingModule } from '@src/app/users/users-routing.module';
     FontAwesomeModule,
     DataTablesModule,
     ComponentsModule,
+    DirectivesModule,
     CoreFormsModule,
     RouterModule,
     CommonModule,
