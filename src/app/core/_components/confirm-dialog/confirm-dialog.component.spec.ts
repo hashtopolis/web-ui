77f5022fb5e65af6fce783e214b9a652f6db787e
gpascal123 <gpascal123@outlook.com>
2025-07-23 08:35:35 +0200

    Error when executing tests fixed.

--- src/app/core/_components/confirm-dialog/confirm-dialog.component.spec.ts
+++ src/app/core/_components/confirm-dialog/confirm-dialog.component.spec.ts
@@ -1,4 +1,6 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 import { ConfirmDialogComponent } from '@components/confirm-dialog/confirm-dialog.component';
 
@@ -8,7 +10,12 @@ describe('ConfirmDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ConfirmDialogComponent]
+      declarations: [ConfirmDialogComponent],
+      providers: [
+        provideHttpClient(),
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ConfirmDialogComponent);
