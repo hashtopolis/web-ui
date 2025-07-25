ecb40c37eec5bfdfebf44af8a4aeaf9902cb3751
gpascal123 <gpascal123@outlook.com>
2025-07-25 08:33:18 +0200

    Added Unit Tests for ui-settings.component

--- src/app/account/settings/ui-settings/ui-settings.component.spec.ts
+++ src/app/account/settings/ui-settings/ui-settings.component.spec.ts
@@ -1,8 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AlertService } from '@services/shared/alert.service';
 
 import { UiSettingsComponent } from '@src/app/account/settings/ui-settings/ui-settings.component';
+import { PageTitleModule } from '@src/app/shared/page-headers/page-title.module';
+import { TableModule } from '@src/app/shared/table/table-actions.module';
+import { UISettingsUtilityClass } from '@src/app/shared/utils/config';
+
 describe('UiSettingsComponent', () => {
   let component: UiSettingsComponent;
   let fixture: ComponentFixture<UiSettingsComponent>;
@@ -10,7 +16,8 @@ describe('UiSettingsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UiSettingsComponent]
+      declarations: [UiSettingsComponent],
+      imports: [MatSelectModule, ReactiveFormsModule, PageTitleModule, TableModule]
     }).compileComponents();
     fixture = TestBed.createComponent(UiSettingsComponent);
     component = fixture.componentInstance;
@@ -31,7 +38,9 @@ describe('UiSettingsComponent', () => {
 
   it('should call updateForm and patch form values', () => {
     spyOn(component.form, 'patchValue');
-    component.util = { uiConfig: { timefmt: 'dd/MM/yyyy h:mm:ss', layout: 'fixed', theme: 'dark' } } as any;
+    component.util = {
+      uiConfig: { timefmt: 'dd/MM/yyyy h:mm:ss', layout: 'fixed', theme: 'dark' }
+    } as UISettingsUtilityClass;
     component.updateForm();
     expect(component.form.patchValue).toHaveBeenCalledWith({
       timefmt: 'dd/MM/yyyy h:mm:ss',
