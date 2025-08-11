34d470fc9fedf0ece91e9b546835841e43243619
cv5ch <176032962+cv5ch@users.noreply.github.com>
2025-08-11 15:45:20 +0200

    Once again try to fix unit test which sometimes runs, sometimes not...

--- src/app/account/settings/acc-settings/acc-settings.component.spec.ts
+++ src/app/account/settings/acc-settings/acc-settings.component.spec.ts
@@ -121,17 +121,14 @@ describe('AccountSettingsComponent', () => {
   });
 
   describe('Main form tests', () => {
-    it('initializes the form with default values', async () => {
-      await fixture.whenStable();
-      fixture.detectChanges();
-
+    it('initializes the form with default values', () => {
       const formValue = component.form.getRawValue();
       expect(formValue.name).toBe(userResponse.attributes.name);
       expect(formValue.email).toBe(userResponse.attributes.email);
       expect(formValue.registeredSince).toBe('08/04/2025 6:25:56');
     });
 
-    it('validates email as required', async () => {
+    it('validates email as required', () => {
       const emailControl = component.form.get('email');
       emailControl?.patchValue(null);
       component.form.updateValueAndValidity();
