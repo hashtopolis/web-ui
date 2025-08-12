3166907fcf4e305d23e6f6862b44e4d9a3026fd5
Niklas <niklas.brynolf@gmail.com>
2025-08-12 08:03:06 +0000

    wip

--- src/app/tasks/new-tasks/new-tasks.component.spec.ts
+++ src/app/tasks/new-tasks/new-tasks.component.spec.ts
@@ -1,20 +1,20 @@
-import { of } from 'rxjs';
-
-import { ChangeDetectorRef } from '@angular/core';
-import { InjectionToken } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
-import { ActivatedRoute, Router } from '@angular/router';
 
-import { GlobalService } from '@src/app/core/_services/main.service';
 import { AlertService } from '@src/app/core/_services/shared/alert.service';
 import { AutoTitleService } from '@src/app/core/_services/shared/autotitle.service';
-import { UIConfigService } from '@src/app/core/_services/shared/storage.service';
-import { TooltipService } from '@src/app/core/_services/shared/tooltip.service';
-import { UnsubscribeService } from '@src/app/core/_services/unsubscribe.service';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
+import { GlobalService } from '@src/app/core/_services/main.service';
+import { InjectionToken } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { NewTasksComponent } from '@src/app/tasks/new-tasks/new-tasks.component';
+import { TooltipService } from '@src/app/core/_services/shared/tooltip.service';
+import { UIConfigService } from '@src/app/core/_services/shared/storage.service';
+import { UnsubscribeService } from '@src/app/core/_services/unsubscribe.service';
 import { getNewTaskForm } from '@src/app/tasks/new-tasks/new-tasks.form';
+import { of } from 'rxjs';
 
 // Create a token for the form factory function
 export const GET_TASK_FORM = new InjectionToken<typeof getNewTaskForm>('getTaskForm');
@@ -100,7 +100,8 @@ describe('NewTasksComponent', () => {
         { provide: Router, useValue: routerSpy },
         // Provide our spy via DI
         { provide: GET_TASK_FORM, useValue: getNewTaskFormSpy }
-      ]
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   });
 
@@ -151,7 +152,6 @@ describe('NewTasksComponent', () => {
       expect(component.form.get('files')).toBeTruthy();
       expect(component.form.get('statusTimer')).toBeTruthy();
     });
-
     /*     it('should initialize form with values from UIConfigService', () => {
       // Reset the form spy to use the actual implementation
       (taskFormModule.getNewTaskForm as jasmine.Spy).and.callThrough();
@@ -164,7 +164,7 @@ describe('NewTasksComponent', () => {
           'tasks.chunkTime': { value: 300 },
           'tasks.statusTimer': { value: 10 },
           'tasks.benchmarkType': { value: 1 },
-          'tasks.color': { value: '#FF0000' }
+          'tasks.color': { value: '#FF0000' } 
         };
         return settings[setting] || null;
       });
