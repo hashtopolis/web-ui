14a31b8d8cd0eee3f487eb239f44d0bbcd005289
cv5ch <176032962+cv5ch@users.noreply.github.com>
2025-08-18 10:54:15 +0200

    Changed ignoreErrors select options to use integer enum as id instead of string

--- src/app/core/_constants/agentsc.config.ts
+++ src/app/core/_constants/agentsc.config.ts
@@ -8,8 +8,17 @@ export class ASC {
   public static CPU_UTIL = 3;
 }
 
-export const ignoreErrors = [
-  { id: 'NO', name: 'Deactivate agent on error' },
-  { id: 'IGNORE_SAVE', name: 'Keep agent running, but save errors' },
-  { id: 'IGNORE_NOSAVE', name: 'Keep agent running and discard errors' }
+/**
+ * Enum for agent ignore errors
+ */
+export enum IgnoreErrors {
+  NO = 0,
+  IGNORE_SAVE = 1,
+  IGNORE_NOSAVE = 2
+}
+
+export const IGNORE_ERROR_CHOICES = [
+  { id: IgnoreErrors.NO, name: 'Deactivate agent on error' },
+  { id: IgnoreErrors.IGNORE_SAVE, name: 'Keep agent running, but save errors' },
+  { id: IgnoreErrors.IGNORE_NOSAVE, name: 'Keep agent running and discard errors' }
 ];
