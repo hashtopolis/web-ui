2d7924ba002d0f540cb2502961642c6a92885b5b
Niklas <niklas.brynolf@gmail.com>
2025-08-26 12:07:05 +0000

    more lint

--- src/app/core/_datasources/vouchers.datasource.ts
+++ src/app/core/_datasources/vouchers.datasource.ts
@@ -1,12 +1,13 @@
 import { catchError, finalize, of } from 'rxjs';
 
-import { BaseDataSource } from '@src/app/core/_datasources/base.datasource';
 import { Filter } from '@models/request-params.model';
+
+import { BaseDataSource } from '@src/app/core/_datasources/base.datasource';
+import { ResponseWrapper } from '@src/app/core/_models/response.model';
 import { JVoucher } from '@src/app/core/_models/voucher.model';
 import { JsonAPISerializer } from '@src/app/core/_services/api/serializer-service';
-import { RequestParamBuilder } from '@src/app/core/_services/params/builder-implementation.service';
-import { ResponseWrapper } from '@src/app/core/_models/response.model';
 import { SERV } from '@src/app/core/_services/main.config';
+import { RequestParamBuilder } from '@src/app/core/_services/params/builder-implementation.service';
 
 export class VouchersDataSource extends BaseDataSource<JVoucher> {
   loadAll(query?: Filter): void {
@@ -32,16 +33,10 @@ export class VouchersDataSource extends BaseDataSource<JVoucher> {
           const length = response.meta.page.total_elements;
           const nextLink = response.links.next;
           const prevLink = response.links.prev;
-          const after = nextLink ? new URL(response.links.next).searchParams.get("page[after]") : null;
-          const before = prevLink ? new URL(response.links.prev).searchParams.get("page[before]") : null;
+          const after = nextLink ? new URL(response.links.next).searchParams.get('page[after]') : null;
+          const before = prevLink ? new URL(response.links.prev).searchParams.get('page[before]') : null;
 
-          this.setPaginationConfig(
-            this.pageSize,
-            length,
-            after,
-            before,
-            this.index
-          );
+          this.setPaginationConfig(this.pageSize, length, after, before, this.index);
           this.setData(vouchers);
         })
     );
