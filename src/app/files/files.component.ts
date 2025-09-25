d725a480247eb906a64e0f7dd95fec0ff1205144
jessevz <jesse.van.zutphen@nfi.nl>
2025-09-25 16:15:44 +0200

    resolved last eslint suggestion

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { FileType } from '@models/file.model';
+
 import { AutoTitleService } from '@services/shared/autotitle.service';
 
 @Component({
