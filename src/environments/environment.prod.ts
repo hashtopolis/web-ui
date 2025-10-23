9e93719c42641110ae10954e15f75d0e9aec5fdf
gluafamichl <>
2025-10-23 10:57:31 +0200

    Fix: set environment.production=true for production build to set backend URL from .env file to localstorage during production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,12 @@
 /**
- * environment definition for development environment.
- * Add a replace section angular.jsom for staging or production environment
+ * Production environment, must have a replace entry in angular.json to be used for production build
  */
 
 import { DEFAULT_CONFIG } from '@src/config/default/app/main';
 import { DEFAULT_CONFIG_TOOLTIP } from '@src/config/default/app/tooltip';
 
 export const environment = {
-  production: false,
+  production: true,
   config: DEFAULT_CONFIG,
   appName: DEFAULT_CONFIG.appName,
   tooltip: DEFAULT_CONFIG_TOOLTIP
