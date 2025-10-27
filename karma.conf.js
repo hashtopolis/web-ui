e2894c648e0497d4b41b1ab37a53b82eb942422e
cv5ch <176032962+cv5ch@users.noreply.github.com>
2025-10-27 15:25:04 +0100

    fix config

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath()
-
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -12,8 +11,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      
+      require('karma-coverage')
     ],
     client: {
       jasmine: {
@@ -30,29 +28,26 @@ module.exports = function (config) {
     },
 
     specReporter: {
-      maxLogLines: 5,             // limit number of lines logged per test
-      suppressSummary: true,      // do not print summary
+      maxLogLines: 5, // limit number of lines logged per test
+      suppressSummary: true, // do not print summary
       suppressErrorSummary: true, // do not print error summary
-      suppressFailed: false,      // do not print information about failed tests
-      suppressPassed: false,      // do not print information about passed tests
-      suppressSkipped: true,      // do not print information about skipped tests
-      showBrowser: false,         // print the browser for each spec
-      showSpecTiming: false,      // print the time elapsed for each spec
-      failFast: false,             // test would finish with error when a first fail occurs
+      suppressFailed: false, // do not print information about failed tests
+      suppressPassed: false, // do not print information about passed tests
+      suppressSkipped: true, // do not print information about skipped tests
+      showBrowser: false, // print the browser for each spec
+      showSpecTiming: false, // print the time elapsed for each spec
+      failFast: false, // test would finish with error when a first fail occurs
       prefixes: {
-        success: '    OK: ',      // override prefix for passed tests, default is '✓ '
-        failure: 'FAILED: ',      // override prefix for failed tests, default is '✗ '
-        skipped: 'SKIPPED: '      // override prefix for skipped tests, default is '- '
+        success: '    OK: ', // override prefix for passed tests, default is '✓ '
+        failure: 'FAILED: ', // override prefix for failed tests, default is '✗ '
+        skipped: 'SKIPPED: ' // override prefix for skipped tests, default is '- '
       }
     },
 
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/hashtopolis'),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
     },
     reporters: ['spec', 'kjhtml'],
     port: 9876,
@@ -61,7 +56,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['ChromeHeadless'],
     singleRun: true,
-    restartOnFileChange: true,
+    restartOnFileChange: true
   });
   process.env.TZ = 'Europe/Berlin';
 };
