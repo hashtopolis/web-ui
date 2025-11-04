993d5fde13d8b992d39c40ecfc5439b769400fd3
gluafamichl <>
2025-11-04 10:31:52 +0100

    Add: Hide notifications tab if read role is missing

---
+++ src/app/core/_services/roles/config/notifications-role.service.ts
@@ -0,0 +1,20 @@
+/**
+ * Role service definition for notifications
+ */
+import { Perm } from '@constants/userpermissions.config';
+
+import { Injectable } from '@angular/core';
+
+import { PermissionService } from '@services/permission/permission.service';
+import { RoleService } from '@services/roles/base/role.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationsRoleService extends RoleService {
+  constructor(permissionService: PermissionService) {
+    super(permissionService, {
+      read: [Perm.Notif.READ]
+    });
+  }
+}
