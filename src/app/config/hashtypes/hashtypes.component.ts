7b0bc5bf29c07b5d99dcd17c4a8f85e51deef40e
cv5ch <176032962+cv5ch@users.noreply.github.com>
2025-12-15 15:32:53 +0100

    Add missing 'create' role to hashtypes role service

--- src/app/config/hashtypes/hashtypes.component.ts
+++ src/app/config/hashtypes/hashtypes.component.ts
@@ -1,13 +1,19 @@
 import { AutoTitleService } from 'src/app/core/_services/shared/autotitle.service';
+
 import { Component } from '@angular/core';
 
+import { HashTypesRoleService } from '@services/roles/config/hashtypes-role.service';
+
 @Component({
-    selector: 'app-hashtypes',
-    templateUrl: './hashtypes.component.html',
-    standalone: false
+  selector: 'app-hashtypes',
+  templateUrl: './hashtypes.component.html',
+  standalone: false
 })
 export class HashtypesComponent {
-  constructor(private titleService: AutoTitleService) {
-    titleService.set(['Show Hashtypes']);
+  constructor(
+    private titleService: AutoTitleService,
+    protected hashtypesRoleService: HashTypesRoleService
+  ) {
+    this.titleService.set(['Show Hashtypes']);
   }
 }
