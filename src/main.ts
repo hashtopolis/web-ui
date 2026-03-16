f16352e748de62a7266a510e04b544da2a336334
correct-horse-battery-bench <l84494245@gmail.com>
2026-03-16 17:08:55 +0100

    Move local-storage and session-storage at first position as these modify global localStorage and sessionStorage

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
+// side effect imports, replace window.localStorage and window.sessionStorage with
+// the typed LocalStorage wrapper and augment the global type.
+// Must run before any other code accesses localStorage or sessionStorage.
+import '@src/app/core/_services/storage/local-storage';
+import '@src/app/core/_services/storage/session-storage';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from '@src/app/app.module';
 import { registerEChartsModules } from '@src/app/shared/graphs/echarts/echarts.config';
 import { environment } from '@src/environments/environment';
-
-// side effect imports, replace window.localStorage and window.sessionStorage with
-// the typed LocalStorage wrapper and augment the global type.
-// Must run before any other code accesses localStorage or sessionStorage.
-import '@src/app/core/_services/storage/local-storage';
-import '@src/app/core/_services/storage/session-storage';
 
 if (environment.production) {
   enableProdMode();
