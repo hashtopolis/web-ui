94da7bf4a2516dabcc43b38501d11223f50964ee
correct-horse-battery-bench <l84494245@gmail.com>
2026-04-09 17:14:39 +0200

    Delete unused pipes

--- src/app/shared/pipes.module.ts
+++ src/app/shared/pipes.module.ts
@@ -2,71 +2,32 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { AgentSColorPipe } from '@src/app/core/_pipes/agentstat-color.pipe';
-import { AveragePipe } from '@src/app/core/_pipes/average-object.pipe';
 import { uiDatePipe } from '@src/app/core/_pipes/date.pipe';
 import { FileSizePipe } from '@src/app/core/_pipes/file-size.pipe';
-import { FileTypePipe } from '@src/app/core/_pipes/file-type.pipe';
-import { FilterItemPipe } from '@src/app/core/_pipes/filter-item.pipe';
-import { SearchPipe } from '@src/app/core/_pipes/filter-search.pipe';
-import { GroupByPipe } from '@src/app/core/_pipes/groupby.pipe';
-import { HashesFilterPipe } from '@src/app/core/_pipes/hashes-filter.pipe';
 import { HashRatePipe } from '@src/app/core/_pipes/hashrate-pipe';
 import { HealthCheckStatusPipe } from '@src/app/core/_pipes/healthcheck-status.pipe';
-import { MaximizePipe } from '@src/app/core/_pipes/maximize-object.pipe';
-import { ArraySortPipe } from '@src/app/core/_pipes/orderby-item.pipe';
-import { ReplaceStringPipe } from '@src/app/core/_pipes/replace-string.pipe';
 import { SecondsToTimePipe } from '@src/app/core/_pipes/secondsto-time.pipe';
-import { ShortenStringPipe } from '@src/app/core/_pipes/shorten-string.pipe';
-import { SplitPipe } from '@src/app/core/_pipes/split.pipe';
 import { StaticArrayPipe } from '@src/app/core/_pipes/static-array.pipe';
-import { SumPipe } from '@src/app/core/_pipes/sum-object.pipe';
-import { WarningColorPipe } from '@src/app/core/_pipes/warning-color.pipe';
 
 @NgModule({
   declarations: [
     HealthCheckStatusPipe,
-    ReplaceStringPipe,
-    ShortenStringPipe,
     SecondsToTimePipe,
-    WarningColorPipe,
-    HashesFilterPipe,
     StaticArrayPipe,
     AgentSColorPipe,
-    FilterItemPipe,
-    ArraySortPipe,
     FileSizePipe,
-    FileTypePipe,
     HashRatePipe,
-    MaximizePipe,
-    AveragePipe,
-    GroupByPipe,
-    SearchPipe,
-    uiDatePipe,
-    SplitPipe,
-    SumPipe
+    uiDatePipe
   ],
   imports: [CommonModule],
   exports: [
     HealthCheckStatusPipe,
-    ReplaceStringPipe,
-    ShortenStringPipe,
     SecondsToTimePipe,
-    WarningColorPipe,
-    HashesFilterPipe,
     StaticArrayPipe,
     AgentSColorPipe,
-    FilterItemPipe,
-    ArraySortPipe,
     FileSizePipe,
     HashRatePipe,
-    FileTypePipe,
-    MaximizePipe,
-    AveragePipe,
-    GroupByPipe,
-    SearchPipe,
-    uiDatePipe,
-    SplitPipe,
-    SumPipe
+    uiDatePipe
   ]
 })
 export class PipesModule {}
