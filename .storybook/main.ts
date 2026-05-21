f4a9ab704e5c57d3f6a780a9a0f1912e1ccea2c1
correct-horse-battery-bench <l84494245@gmail.com>
2026-05-21 08:25:58 +0200

    chore: remove Storybook setup
    Storybook is no longer used in this project. Removes the configuration
    directory, all *.stories.ts files and fixtures, the Angular builder
    targets, the npm scripts, and the corresponding devDependencies
    (including @compodoc/compodoc and eslint-plugin-storybook).

--- .storybook/main.ts
+++
@@ -1,9 +0,0 @@
-import type { StorybookConfig } from '@storybook/angular';
-
-const config: StorybookConfig = {
-  stories: ['../src/stories/**/*.stories.@(js|ts)'],
-  addons: ['@storybook/addon-a11y', '@storybook/addon-docs'],
-  framework: '@storybook/angular',
-  staticDirs: ['../src/assets']
-};
-export default config;
--- .storybook/typings.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.md' {
-  const content: string;
-  export default content;
-}
-
-declare module 'pdfmake/build/pdfmake';
-declare module 'pdfmake/build/vfs_fonts';
