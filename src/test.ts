0f0413b297959dd73e2015af4e2f4071fae98d68
correct-horse-battery-bench <l84494245@gmail.com>
2026-05-22 11:18:12 +0200

    Potentially fix tests after angular 20 -> 21 upgrade

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,36 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/testing';
-import { getTestBed } from '@angular/core/testing';
+import { NgModule, provideZoneChangeDetection } from '@angular/core';
+import { getTestBed, TestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
+@NgModule({
+  providers: [provideZoneChangeDetection()]
+})
+export class ZoneTestingModule {}
+
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
+  [BrowserDynamicTestingModule, ZoneTestingModule],
   platformBrowserDynamicTesting(),
 );
+
+// Patch TestBed.configureTestingModule to restore legacy zone behavior in Angular 21 tests
+const originalConfigureTestingModule = TestBed.configureTestingModule;
+
+TestBed.configureTestingModule = (moduleDef) => {
+  return originalConfigureTestingModule.call(TestBed, {
+    ...moduleDef,
+    rethrowApplicationErrors: false,
+    providers: [
+      ...(moduleDef.providers || []),
+      provideZoneChangeDetection(),
+    ],
+  });
+};
+
+
