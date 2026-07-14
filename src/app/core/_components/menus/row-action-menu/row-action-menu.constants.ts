77848db3195577aa04435fd79bc5655554d1ac1d
Inadequate Immortal jellyfish <Devalue9255@proton.me>
2026-07-14 11:04:32 +0200

    2302 bug edit task page assigned agents table has no possibility to unassign agent from task (#762)

--- src/app/core/_components/menus/row-action-menu/row-action-menu.constants.ts
+++ src/app/core/_components/menus/row-action-menu/row-action-menu.constants.ts
@@ -84,6 +84,7 @@ export const RowActionMenuAction = {
   DEACTIVATE: 'deactivate',
   DOWNLOAD: 'download',
   UNARCHIVE: 'unarchive',
+  UNASSIGN: 'unassign',
   RESET: 'reset',
   VIEW: 'view'
 };
